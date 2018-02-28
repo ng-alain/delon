69b2bf3715eadc2a0a36a66bcb6e97490c5c22b5
cipchk <cipchk@qq.com>
2018-02-28 16:06:35 +0800

    feat(cli): add module no-route placeholder

--- src/core/cli/module/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.module.ts
+++ src/core/cli/module/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 <% if (routing) { %>import { <%= classify(name) %>RoutingModule } from './<%= dasherize(name) %>-routing.module';<% } %>
 
+const COMPONENT_NOROUNT = [];
+
 @NgModule({
   imports: [
     SharedModule,<% if (routing) { %>
     <%= classify(name) %>RoutingModule<% } %>
   ],
-  declarations: []
+  declarations: [
+      ...COMPONENT_NOROUNT
+  ],
+  entryComponents: COMPONENT_NOROUNT
 })
 export class <%= classify(name) %>Module { }
