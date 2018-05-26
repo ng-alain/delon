d345120ddd9567e711be59fe756a5753b1679a5d
cipchk <cipchk@qq.com>
2018-05-26 22:33:50 +0800

    docs: fix missing stackblitz

--- site/app/core/code.service.ts
+++ site/app/core/code.service.ts
@@ -6,27 +6,7 @@ import { NzModalService } from 'ng-zorro-antd';
 export class CodeService {
   constructor(private modal: NzModalService) {}
 
-  /** bug here https://github.com/stackblitz/core/issues/311 **/
   openOnStackBlitz(code: string, title: string, summary: string) {
-    if (~code.indexOf('@delon/form')) {
-      this.modal
-        .confirm(
-          {
-            nzTitle: `Stackblita 暂不支持 external resources 导入，需要手工加入否则会出现 Ajv 不存在问题`,
-            nzContent: `https://cdn.bootcss.com/ajv/6.4.0/ajv.min.js`,
-            nzOnOk: () => this._openOnStackBlitz(code, title, summary),
-          },
-          'info',
-        )
-        .open();
-      return;
-    }
-
-    this._openOnStackBlitz(code, title, summary);
-  }
-
-  /** bug here https://github.com/stackblitz/core/issues/311 **/
-  private _openOnStackBlitz(code: string, title: string, summary: string) {
     let selector = '',
       componentName = '';
     const selectorRe = /selector:[ ]?(['|"|`])([^'"`]+)/g.exec(code);
@@ -43,12 +23,132 @@ export class CodeService {
       description: `${title}-${summary.replace(/<[^>]+>/g, '')}`,
       tags: ['ng-alain', '@delon'],
       files: {
-        '.angular-cli.json': `{
-    "apps": [{
-      "styles": ["styles.less"]
-    }]
-  }`,
-        'index.html': [
+        'angular.json': `{
+  "$schema": "./node_modules/@angular/cli/lib/config/schema.json",
+  "version": 1,
+  "newProjectRoot": "projects",
+  "projects": {
+    "demo": {
+      "root": "",
+      "sourceRoot": "src",
+      "projectType": "application",
+      "prefix": "app",
+      "schematics": {},
+      "architect": {
+        "build": {
+          "builder": "@angular-devkit/build-angular:browser",
+          "options": {
+            "outputPath": "dist/demo",
+            "index": "src/index.html",
+            "main": "src/main.ts",
+            "polyfills": "src/polyfills.ts",
+            "tsConfig": "src/tsconfig.app.json",
+            "assets": [
+              "src/favicon.ico",
+              "src/assets"
+            ],
+            "styles": [
+              "node_modules/@delon/theme/styles/ng-alain.css",
+              "src/styles.less"
+            ],
+            "scripts": [
+            ]
+          },
+          "configurations": {
+            "production": {
+              "fileReplacements": [
+                {
+                  "replace": "src/environments/environment.ts",
+                  "with": "src/environments/environment.prod.ts"
+                }
+              ],
+              "optimization": true,
+              "outputHashing": "all",
+              "sourceMap": false,
+              "extractCss": true,
+              "namedChunks": false,
+              "aot": true,
+              "extractLicenses": true,
+              "vendorChunk": false,
+              "buildOptimizer": true
+            }
+          }
+        },
+        "serve": {
+          "builder": "@angular-devkit/build-angular:dev-server",
+          "options": {
+            "browserTarget": "demo:build"
+          },
+          "configurations": {
+            "production": {
+              "browserTarget": "demo:build:production"
+            }
+          }
+        },
+        "extract-i18n": {
+          "builder": "@angular-devkit/build-angular:extract-i18n",
+          "options": {
+            "browserTarget": "demo:build"
+          }
+        },
+        "test": {
+          "builder": "@angular-devkit/build-angular:karma",
+          "options": {
+            "main": "src/test.ts",
+            "polyfills": "src/polyfills.ts",
+            "tsConfig": "src/tsconfig.spec.json",
+            "karmaConfig": "src/karma.conf.js",
+            "styles": [
+              "src/styles.less"
+            ],
+            "scripts": [],
+            "assets": [
+              "src/favicon.ico",
+              "src/assets"
+            ]
+          }
+        },
+        "lint": {
+          "builder": "@angular-devkit/build-angular:tslint",
+          "options": {
+            "tsConfig": [
+              "src/tsconfig.app.json",
+              "src/tsconfig.spec.json"
+            ],
+            "exclude": [
+              "**/node_modules/**"
+            ]
+          }
+        }
+      }
+    },
+    "demo-e2e": {
+      "root": "e2e/",
+      "projectType": "application",
+      "architect": {
+        "e2e": {
+          "builder": "@angular-devkit/build-angular:protractor",
+          "options": {
+            "protractorConfig": "e2e/protractor.conf.js",
+            "devServerTarget": "demo:serve"
+          }
+        },
+        "lint": {
+          "builder": "@angular-devkit/build-angular:tslint",
+          "options": {
+            "tsConfig": "e2e/tsconfig.e2e.json",
+            "exclude": [
+              "**/node_modules/**"
+            ]
+          }
+        }
+      }
+    }
+  },
+  "defaultProject": "demo"
+}
+`,
+        'src/index.html': [
           ~code.indexOf('<g2')
             ? `
 <script type="text/javascript" src="https://gw.alipayobjects.com/os/antv/assets/g2/3.0.5-beta.5/g2.min.js"></script>
@@ -59,7 +159,7 @@ export class CodeService {
 
           `<${selector}>loading</${selector}>`,
         ].join(''),
-        'main.ts': `import './polyfills';
+        'src/main.ts': `import './polyfills';
   import { enableProdMode } from '@angular/core';
   import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
   import { AppModule } from './app/app.module';
@@ -71,73 +171,61 @@ export class CodeService {
     window['ngRef'] = ref;
     // Otherise, log the boot error
   }).catch(err => console.error(err));`,
-        'polyfills.ts': `/**
-   * This file includes polyfills needed by Angular and is loaded before the app.
-   * You can add your own extra polyfills to this file.
-   *
-   * This file is divided into 2 sections:
-   *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
-   *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
-   *      file.
-   *
-   * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
-   * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
-   * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
-   *
-   * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
-   */
-  /***************************************************************************************************
-   * BROWSER POLYFILLS
-   */
-  /** IE9, IE10 and IE11 requires all of the following polyfills. **/
-  // import 'core-js/es6/symbol';
-  // import 'core-js/es6/object';
-  // import 'core-js/es6/function';
-  // import 'core-js/es6/parse-int';
-  // import 'core-js/es6/parse-float';
-  // import 'core-js/es6/number';
-  // import 'core-js/es6/math';
-  // import 'core-js/es6/string';
-  // import 'core-js/es6/date';
-  // import 'core-js/es6/array';
-  // import 'core-js/es6/regexp';
-  // import 'core-js/es6/map';
-  // import 'core-js/es6/set';
-  /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-  // import 'classlist.js';  // Run \`npm install --save classlist.js\`.
-  /** IE10 and IE11 requires the following to support \`@angular/animation\`. */
-  // import 'web-animations-js';  // Run \`npm install --save web-animations-js\`.
-  /** Evergreen browsers require these. **/
-  import 'core-js/es6/reflect';
-  import 'core-js/es7/reflect';
-  /** ALL Firefox browsers require the following to support \`@angular/animation\`. **/
-  // import 'web-animations-js';  // Run \`npm install --save web-animations-js\`.
-  /***************************************************************************************************
-   * Zone JS is required by Angular itself.
-   */
-  import 'zone.js/dist/zone';  // Included with Angular CLI.
-  /***************************************************************************************************
-   * APPLICATION IMPORTS
-   */
-  /**
-   * Date, currency, decimal and percent pipes.
-   * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
-   */
-  // import 'intl';  // Run \`npm install --save intl\`.`,
-        'app/app.component.ts': code,
-        'app/app.module.ts': `import { NgModule } from '@angular/core';
+        'src/polyfills.ts': `/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+// import 'core-js/es6/symbol';
+// import 'core-js/es6/object';
+// import 'core-js/es6/function';
+// import 'core-js/es6/parse-int';
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
+// import 'core-js/es6/date';
+// import 'core-js/es6/array';
+// import 'core-js/es6/regexp';
+// import 'core-js/es6/map';
+// import 'core-js/es6/set';
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+// import 'classlist.js';  // Run \`npm install --save classlist.js\`.
+/** IE10 and IE11 requires the following to support \`@angular/animation\`. */
+// import 'web-animations-js';  // Run \`npm install --save web-animations-js\`.
+/** Evergreen browsers require these. **/
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+// import 'web-animations-js';
+import 'zone.js/dist/zone';`,
+        'src/app/app.component.ts': code,
+        'src/app/app.module.ts': `import { NgModule, APP_INITIALIZER, Injectable } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { AlainThemeModule } from '@delon/theme';
 import { DelonABCModule } from '@delon/abc';
+import { DelonFormModule } from '@delon/form';
 import { DelonAuthModule } from '@delon/auth';
 import { DelonACLModule } from '@delon/acl';
 import { DelonCacheModule } from '@delon/cache';
-import { DelonFormModule } from '@delon/form';
+import { DelonUtilModule, LazyService } from '@delon/util';
+
+@Injectable()
+export class StartupService {
+  constructor(private lazy: LazyService) { }
+  load(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.lazy.load([
+        'https://cdn.bootcss.com/ajv/6.4.0/ajv.min.js'
+      ])
+        .then(() => resolve(null));
+    });
+  }
+}
+
+export function StartupServiceFactory(startupService: StartupService): Function {
+  return () => startupService.load();
+}
 
 import { ${componentName} } from './app.component';
 
@@ -146,7 +234,6 @@ imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule,
     BrowserAnimationsModule,
     NgZorroAntdModule.forRoot(),
     AlainThemeModule.forRoot(),
@@ -155,42 +242,50 @@ imports: [
     DelonACLModule.forRoot(),
     DelonCacheModule.forRoot(),
     DelonFormModule.forRoot(),
+    DelonUtilModule.forRoot(),
+],
+providers: [
+  StartupService,
+  {
+    provide: APP_INITIALIZER,
+    useFactory: StartupServiceFactory,
+    deps: [StartupService],
+    multi: true
+  }
 ],
 declarations: [ ${componentName} ],
 bootstrap:    [ ${componentName} ]
 })
 export class AppModule { }
   `,
-        'styles.less': `@import "~ng-zorro-antd/src/ng-zorro-antd.less";
-@import '~@delon/theme/styles/index';
-@import '~@delon/abc/index';
-
-// 例如：内容区域背景色为白色
-// @content-bg:  #fff;`,
+        'src/styles.less': ``,
       },
       template: 'angular-cli',
       dependencies: {
-        '@angular/cdk': '^5.0.0',
-        '@angular/core': '^5.0.0',
-        '@angular/forms': '^5.0.0',
-        '@angular/http': '^5.0.0',
-        '@angular/language-service': '^5.0.0',
-        '@angular/platform-browser': '^5.0.0',
-        '@angular/platform-browser-dynamic': '^5.0.0',
-        '@angular/common': '^5.0.0',
-        '@angular/router': '^5.0.0',
-        '@angular/animations': '^5.0.0',
-        'date-fns': '^1.29.0',
+        '@angular/cdk': '*',
+        '@angular/core': '*',
+        '@angular/forms': '*',
+        '@angular/http': '*',
+        '@angular/language-service': '*',
+        '@angular/platform-browser': '*',
+        '@angular/platform-browser-dynamic': '*',
+        '@angular/common': '*',
+        '@angular/router': '*',
+        '@angular/animations': '*',
+        'date-fns': '*',
         'file-saver': '^1.3.3',
-        'ngx-countdown': '^2.0.0',
-        'ng-zorro-antd': 'next',
-        '@delon/theme': 'next',
-        '@delon/abc': 'next',
-        '@delon/acl': 'next',
-        '@delon/auth': 'next',
-        '@delon/cache': 'next',
-        '@delon/mock': 'next',
-        '@delon/form': 'next',
+        'ngx-countdown': '*',
+        'ng-zorro-antd': '*',
+        '@delon/theme': '*',
+        '@delon/abc': '*',
+        '@delon/acl': '*',
+        '@delon/auth': '*',
+        '@delon/cache': '*',
+        '@delon/mock': '*',
+        '@delon/form': '*',
+        '@delon/util': '*',
+        'extend': '*',
+        'rxjs-compat': '*',
       },
     });
   }
