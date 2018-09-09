97244b96f2abd1bec38fe683e8186e430906622e
cipchk <cipchk@qq.com>
2018-09-09 12:48:57 +0800

    fix(abc:desc-list): fix missing export AdDescListConfig, close #154

--- packages/abc/desc-list/desc-list.config.ts
+++ packages/abc/desc-list/desc-list.config.ts
@@ -1,6 +1,3 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
 export class AdDescListConfig {
   /**
    * 布局方式
--- packages/abc/desc-list/public_api.ts
+++ packages/abc/desc-list/public_api.ts
@@ -1,3 +1,4 @@
 export { DescListComponent } from './desc-list.component';
 export { DescListItemComponent } from './desc-list-item.component';
+export { AdDescListConfig } from './desc-list.config';
 export { AdDescListModule } from './desc-list.module';
