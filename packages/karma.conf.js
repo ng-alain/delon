f611f7f556d5294970ae42d0f497942251821728
cipchk <cipchk@qq.com>
2018-12-13 13:57:37 +0800

    chore: test add parallel of karma

--- packages/karma.conf.js
+++ packages/karma.conf.js
@@ -2,17 +2,28 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
+  const executors = (Math.ceil(require('os').cpus().length / 2));
+  console.log(`executors cpus: `, executors);
   const configuration = {
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: [
+      'parallel',
+      'jasmine',
+      '@angular-devkit/build-angular'
+    ],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-summary-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-parallel'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    parallelOptions: {
+      executors,
+      shardStrategy: 'round-robin'
+    },
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
       timeout: 180000
