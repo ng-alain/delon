9eb38b566d819c3f1b5cdea59bf4e650dc3f2d67
cipchk <cipchk@qq.com>
2019-04-11 09:49:48 +0700

    feat(form): add loading property
    - close https://github.com/ng-alain/ng-alain/issues/1017

--- src/app/routes/dev/demo/demo.component.ts
+++ src/app/routes/dev/demo/demo.component.ts
@@ -1,37 +1,18 @@
+
 import { Component } from '@angular/core';
 import { SFSchema } from '@delon/form';
 import { NzMessageService } from 'ng-zorro-antd';
 
-const r = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min);
-
 @Component({
   selector: 'app-demo',
-  template: `
-  <div style="width: 200px; display: inline-block">
-      <g2-pie
-          [percent]="percent"
-          subTitle="中式快餐"
-          total="28%"
-          height="130"></g2-pie>
-  </div>
-  <button nz-button (click)="change()">change</button>
-  `,
+  template: `<sf [schema]="schema" [loading]="loading" (formSubmit)="submit($event)"></sf>`,
 })
-export class DemoComponent  {
-  percent = 0;
-  change() {
-    this.percent = r(0, 100);
-  }
-
-  // @delon/form
+export class DemoComponent {
+  loading = false;
   schema: SFSchema = {
-    definitions: {
-      nameRef: {
-        type: 'string',
-        title: 'nameRef',
-      },
-    },
     properties: {
+      id1: { type: 'number', ui: { widget: 'text' } },
+      id2: { type: 'number', ui: { widget: 'text', defaultText: 'default text' } },
       name: {
         type: 'string',
         title: 'Name',
@@ -40,12 +21,15 @@ export class DemoComponent  {
           placeholder: 'RMB结算',
         },
       },
-      nameTwo: {
-        '$ref': '#/definitions/nameRef',
-      },
     },
-    required: ['name', 'nameTwo'],
+    required: ['name'],
   };
   constructor(public msg: NzMessageService) { }
-  submit(value: any) { this.msg.success(JSON.stringify(value)); }
+  submit(value: any) {
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+      this.msg.success(JSON.stringify(value));
+    }, 1000);
+   }
 }
