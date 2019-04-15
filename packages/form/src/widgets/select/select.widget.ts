2302aa8b3398d0067d7b7371e8d7b9e28ab3f0de
cipchk <cipchk@qq.com>
2019-04-15 08:41:49 +0700

    fix(form:select): fix i18n of notFoundContent
    - close https://github.com/ng-alain/ng-alain/issues/1025

--- packages/form/src/widgets/select/select.widget.ts
+++ packages/form/src/widgets/select/select.widget.ts
@@ -24,7 +24,7 @@ export class SelectWidget extends ControlWidget implements OnInit {
       serverSearch: toBool(this.ui.serverSearch, false),
       maxMultipleCount: this.ui.maxMultipleCount || Infinity,
       mode: this.ui.mode || 'default',
-      notFoundContent: this.ui.notFoundContent || '无法找到',
+      notFoundContent: this.ui.notFoundContent,
       showSearch: toBool(this.ui.showSearch, true),
       tokenSeparators: this.ui.tokenSeparators || [],
       maxTagCount: this.ui.maxTagCount || null,
