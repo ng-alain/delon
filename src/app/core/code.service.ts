6d35fc46797e837ae52b981bda7ae9f715a71158
卡色 <cipchk@qq.com>
2019-04-29 20:23:37 +0700

    chore: G2 is not defined (#544)

--- src/app/core/code.service.ts
+++ src/app/core/code.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import sdk from '@stackblitz/sdk';
+import * as pkg from '../../../package.json';
 
 @Injectable({ providedIn: 'root' })
 export class CodeService {
@@ -14,7 +15,8 @@ export class CodeService {
     if (componentNameRe) {
       componentName = componentNameRe[1];
     }
-    const isG2 = ~code.indexOf('<g2');
+    const isG2 = code.includes('<g2');
+    let g2Libs: string[] = [];
 
     if (isG2) {
       code =
@@ -22,8 +24,12 @@ export class CodeService {
 declare var G2: any;
 declare var DataSet: any;
 declare var Slider: any;
-
-` + code;
+ ` + code;
+      g2Libs = [
+        `'https://unpkg.com/@antv/g2@${pkg.dependencies['@antv/g2'].substr(1)}/dist/g2.min.js'`,
+        `'https://unpkg.com/@antv/data-set@${pkg.dependencies['@antv/data-set'].substr(1)}/dist/data-set.min.js'`,
+        `'https://unpkg.com/@antv/g2-plugin-slider@${pkg.dependencies['@antv/g2-plugin-slider'].substr(1)}/dist/g2-plugin-slider.min.js'`,
+      ];
     }
 
     sdk.openProject(
@@ -158,14 +164,6 @@ declare var Slider: any;
 }
 `,
           'src/index.html': [
-            ~isG2
-              ? `
-<script type="text/javascript" src="https://gw.alipayobjects.com/os/antv/pkg/_antv.g2-3.4.1/dist/g2.min.js"></script>
-<script type="text/javascript" src="https://gw.alipayobjects.com/os/antv/pkg/_antv.data-set-0.10.1/dist/data-set.min.js"></script>
-<script type="text/javascript" src="https://gw.alipayobjects.com/os/antv/assets/g2-plugin-slider/2.0.0/g2-plugin-slider.js"></script>
-`
-              : ``,
-
             `<${selector}>loading</${selector}>
 <div id="VERSION" style="position: fixed; bottom: 8px; right: 8px; z-index: 8888;"></div>
           `,
@@ -237,7 +235,8 @@ export class StartupService {
   load(): Promise<any> {
     return new Promise((resolve, reject) => {
       this.lazy.load([
-        'https://cdnjs.cloudflare.com/ajax/libs/ajv/6.10.0/ajv.min.js'
+        'https://cdnjs.cloudflare.com/ajax/libs/ajv/${pkg.dependencies.ajv.substr(1)}/ajv.min.js',
+        ${isG2 ? g2Libs.join(',') : '' }
       ])
         .then(() => resolve(null));
     });
