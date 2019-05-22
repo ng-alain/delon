decb70bbb7dd6056ec8da0b7779bf186999486ce
cipchk <cipchk@qq.com>
2019-05-22 08:42:14 +0700

    fix(abc:se): fix losing opttional icon

--- packages/abc/edit/edit.module.ts
+++ packages/abc/edit/edit.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DelonUtilModule } from '@delon/util';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
 import { SEContainerComponent } from './edit-container.component';
@@ -11,7 +12,7 @@ import { SEComponent } from './edit.component';
 const COMPONENTS = [SEContainerComponent, SEComponent, SEErrorComponent, SETitleComponent];
 
 @NgModule({
-  imports: [CommonModule, DelonUtilModule, NzToolTipModule],
+  imports: [CommonModule, DelonUtilModule, NzToolTipModule, NzIconModule],
   declarations: [...COMPONENTS],
   exports: [...COMPONENTS],
 })
