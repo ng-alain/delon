641a72c8321f56212dc200632b85388afee04bc5
cipchk <cipchk@qq.com>
2019-06-28 09:26:10 +0800

    chore: fix test

--- packages/abc/ellipsis/ellipsis.component.ts
+++ packages/abc/ellipsis/ellipsis.component.ts
@@ -175,7 +175,6 @@ export class EllipsisComponent implements AfterViewInit, OnChanges {
         const count = this.bisection(targetHeight, mid, 0, len, lineText, shadowTextEl.nativeElement.firstChild);
         this.text = lineText;
         this.targetCount = count;
-        console.log(lineHeight, targetHeight, len, mid, count);
       }
       cdr.detectChanges();
     }
