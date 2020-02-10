50d8b50f919ca77fa9339ba21b23a50bf4bb0108
cipchk <cipchk@qq.com>
2020-02-10 10:39:50 +0800

    chore: fix

--- packages/abc/table/test/table-column-source.spec.ts
+++ packages/abc/table/test/table-column-source.spec.ts
@@ -13,11 +13,6 @@ class MockI18NServiceFake extends AlainI18NServiceFake {
     return i18nResult;
   }
 }
-class MockDomSanitizer {
-  bypassSecurityTrustHtml(text: string) {
-    return text;
-  }
-}
 
 describe('st: column-source', () => {
   let aclSrv: ACLService | null;
@@ -30,7 +25,7 @@ describe('st: column-source', () => {
     aclSrv = other.acl ? new ACLService({}) : null;
     i18nSrv = other.i18n ? new MockI18NServiceFake() : null;
     rowSrv = new STRowSource();
-    srv = new STColumnSource(new MockDomSanitizer() as any, rowSrv, aclSrv!, i18nSrv!, other.cog || new STConfig());
+    srv = new STColumnSource(rowSrv, aclSrv!, i18nSrv!, other.cog || new STConfig());
     page = new PageObject();
   }
 
@@ -90,7 +85,10 @@ describe('st: column-source', () => {
         });
         it('should be throw error when mulit column', () => {
           expect(() => {
-            srv.process([{ title: '1', index: 'id', type: 'checkbox' }, { title: '2', index: 'id', type: 'checkbox' }]);
+            srv.process([
+              { title: '1', index: 'id', type: 'checkbox' },
+              { title: '2', index: 'id', type: 'checkbox' },
+            ]);
           }).toThrow();
         });
         it('should auto 50px width when without specified with value', () => {
@@ -117,7 +115,10 @@ describe('st: column-source', () => {
       describe(`with radio`, () => {
         it('should be throw error when mulit column', () => {
           expect(() => {
-            srv.process([{ title: '1', index: 'id', type: 'radio' }, { title: '2', index: 'id', type: 'radio' }]);
+            srv.process([
+              { title: '1', index: 'id', type: 'radio' },
+              { title: '2', index: 'id', type: 'radio' },
+            ]);
           }).toThrow();
         });
         it('should auto 50px width when without specified with value', () => {
@@ -535,15 +536,17 @@ describe('st: column-source', () => {
           },
         ])[0].filter!.menus!.length,
       ).toBe(1);
-      expect(srv.process([
-        {
-          title: '',
-          index: 'id',
-          filter: {
-            menus: [{ text: '1', acl: 'admin' }],
+      expect(
+        srv.process([
+          {
+            title: '',
+            index: 'id',
+            filter: {
+              menus: [{ text: '1', acl: 'admin' }],
+            },
           },
-        },
-      ])[0]!.filter as any).toBe(null);
+        ])[0]!.filter as any,
+      ).toBe(null);
     });
 
     it('in buttons', () => {
