d4566d71e31a3a8a3ce59627709ed86db2d2e5b1
卡色 <cipchk@qq.com>
2020-04-07 13:04:11 +0800

    fix(abc:page-header): fix missing import of `@angular/cdk/observ… (#789)
    - close https://github.com/ng-alain/ng-alain/issues/1526

--- packages/abc/page-header/page-header.module.ts
+++ packages/abc/page-header/page-header.module.ts
@@ -5,13 +5,14 @@ import { DelonUtilModule } from '@delon/util';
 import { NzAffixModule } from 'ng-zorro-antd/affix';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
+import { ObserversModule } from '@angular/cdk/observers';
 
 import { PageHeaderComponent } from './page-header.component';
 
 const COMPONENTS = [PageHeaderComponent];
 
 @NgModule({
-  imports: [CommonModule, RouterModule, DelonUtilModule, NzAffixModule, NzSkeletonModule, NzBreadCrumbModule],
+  imports: [CommonModule, ObserversModule, RouterModule, DelonUtilModule, NzAffixModule, NzSkeletonModule, NzBreadCrumbModule],
   declarations: [...COMPONENTS],
   exports: [...COMPONENTS],
 })
