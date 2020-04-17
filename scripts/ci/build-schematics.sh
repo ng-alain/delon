59d7cd0ddb7d202be420160222f99e88d27b654c
cipchk <cipchk@qq.com>
2020-04-18 00:52:15 +0800

    chore: update

--- scripts/ci/build-schematics.sh
+++ scripts/ci/build-schematics.sh
@@ -115,9 +115,9 @@ copyFiles() {
     "${1}.prettierignore|${2}application/files/root/__dot__prettierignore"
     "${1}.prettierrc|${2}application/files/root/__dot__prettierrc"
     "${1}.stylelintrc|${2}application/files/root/__dot__stylelintrc"
+    "${1}.lintstagedrc.js|${2}application/files/root"
     "${1}tslint.json|${2}application/files/root"
     "${1}proxy.conf.json|${2}application/files/root"
-    "${1}lint-staged.config.js|${2}application/files/root"
     # cli
     # "${1}_cli-tpl|${2}application/files/root/"
     # ci
