f07636a99106af8620e4a008649a77129248fa57
cipchk <cipchk@qq.com>
2020-04-24 23:12:07 +0800

    chore: update

--- src/app/core/code/code.service.ts
+++ src/app/core/code/code.service.ts
@@ -7,6 +7,8 @@ import angularJSON from './files/angular.json';
 import appModuleTS from './files/app.module';
 import delonABCModuleTS from './files/delon-abc.module';
 import delonChartModuleTS from './files/delon-chart.module';
+import dotAngularCliJSON from './files/dot_angular-cli.json';
+import environmentTS from './files/environment';
 import mainTS from './files/main';
 import nzZorroAntdModuleTS from './files/ng-zorro-antd.module';
 import polyfillTS from './files/polyfill';
@@ -30,6 +32,9 @@ export class CodeService {
       '@angular/router': '^9.0.0',
       '@ant-design/icons-angular': '9.0.1',
       'core-js': '3.6.4',
+      rxjs: '6.5.4',
+      tslib: '1.11.1',
+      'zone.js': '0.10.2',
       '@antv/g2': '*',
       '@antv/data-set': '*',
       'date-fns': '*',
@@ -58,7 +63,7 @@ export class CodeService {
     return startupServiceTS({ ajvVersion: pkg.dependencies.ajv.substr(1) });
   }
 
-  private get genMock(): {} {
+  private get genMock(): { [key: string]: string } {
     return { '_mock/user.ts': require('!!raw-loader!../../../../_mock/user.ts').default, '_mock/index.ts': `export * from './user';` };
   }
 
@@ -76,7 +81,11 @@ export class CodeService {
     return {
       selector,
       componentName,
-      html: `<${selector}>loading</${selector}><div id="VERSION" style="position: fixed; bottom: 8px; right: 8px; z-index: 8888;"></div>`,
+      html: [
+        `<base href="/">`,
+        `<${selector}>loading</${selector}>`,
+        `<div id="VERSION" style="position: fixed; bottom: 8px; right: 8px; z-index: 8888;"></div>`,
+      ].join('\n'),
     };
   }
 
@@ -91,6 +100,7 @@ export class CodeService {
         dependencies: this.dependencies,
         files: {
           'angular.json': `${JSON.stringify(angularJSON)}`,
+          'environments/environment.ts': environmentTS,
           'src/index.html': res.html,
           'src/main.ts': mainTS,
           'src/polyfills.ts': polyfillTS,
@@ -113,7 +123,7 @@ export class CodeService {
 
   openOnCodeSandbox(appComponentCode: string) {
     const res = this.parseCode(appComponentCode);
-    console.log(res);
+    const mockObj = this.genMock;
     const parameters = getParameters({
       files: {
         'package.json': {
@@ -126,13 +136,69 @@ export class CodeService {
           ),
           isBinary: false,
         },
+        '.angular-cli.json': {
+          content: dotAngularCliJSON,
+          isBinary: false,
+        },
+        'environments/environment.ts': {
+          content: environmentTS,
+          isBinary: false,
+        },
+        'src/index.html': {
+          content: res.html,
+          isBinary: false,
+        },
+        'src/main.ts': {
+          content: mainTS,
+          isBinary: false,
+        },
+        'src/polyfills.ts': {
+          content: polyfillTS,
+          isBinary: false,
+        },
+        'src/app/app.module.ts': {
+          content: appModuleTS(res.componentName),
+          isBinary: false,
+        },
+        'src/app/app.component.ts': {
+          content: appComponentCode,
+          isBinary: false,
+        },
+        'src/app/ng-zorro-antd.module.ts': {
+          content: nzZorroAntdModuleTS,
+          isBinary: false,
+        },
+        'src/app/delon-abc.module.ts': {
+          content: delonABCModuleTS,
+          isBinary: false,
+        },
+        'src/app/delon-chart.module.ts': {
+          content: delonChartModuleTS,
+          isBinary: false,
+        },
+        'src/app/startup.service.ts': {
+          content: this.genStartupService,
+          isBinary: false,
+        },
+        'src/styles.css': {
+          content: ``,
+          isBinary: false,
+        },
+        '_mock/user.ts': {
+          content: mockObj['_mock/user.ts'],
+          isBinary: false,
+        },
+        '_mock/index.ts': {
+          content: mockObj['_mock/index.ts'],
+          isBinary: false,
+        },
       },
     });
 
     const form = this.document.createElement('form');
     const parametersInput = this.document.createElement('input');
     form.method = 'POST';
-    form.action = 'https://codesandbox.io/api/v1/sandboxes/define?module=/src/app/app.component.ts';
+    form.action = 'https://codesandbox.io/api/v1/sandboxes/define';
     form.target = '_blank';
     parametersInput.name = 'parameters';
     parametersInput.value = parameters;
