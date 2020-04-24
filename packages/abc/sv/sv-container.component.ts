8bad0d7a0b0ccafddfcead4250178923114d9ccf
cipchk <cipchk@qq.com>
2020-04-24 23:24:53 +0800

    refactor: removed config

--- packages/abc/sv/sv-container.component.ts
+++ packages/abc/sv/sv-container.component.ts
@@ -24,7 +24,7 @@ export class SVContainerComponent {
   /** 列表项间距，单位为 `px` */
   @Input() @InputNumber() gutter: number;
   @Input() layout: 'horizontal' | 'vertical';
-  @Input() @InputNumber(null) labelWidth: number;
+  @Input() @InputNumber() labelWidth: number;
   /** 指定信息最多分几列展示，最终一行几列由 col 配置结合响应式规则决定 */
   @Input() @InputNumber() col: number;
   @Input() default: boolean;
