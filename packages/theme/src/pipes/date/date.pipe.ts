34cde817ef3b9f6c017237af6866b5cb0c64b988
cipchk <cipchk@qq.com>
2020-04-27 14:24:24 +0800

    chore: fix test

--- packages/theme/src/pipes/date/date.pipe.ts
+++ packages/theme/src/pipes/date/date.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { toDate, ToDateOptions } from '@delon/util';
+import { toDate } from '@delon/util';
 import format from 'date-fns/format';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import { NzSafeAny } from 'ng-zorro-antd/core/types';
@@ -9,11 +9,10 @@ import { NzI18nService } from 'ng-zorro-antd/i18n';
 export class DatePipe implements PipeTransform {
   constructor(private nzI18n: NzI18nService) {}
 
-  transform(value: Date | string | number, options: ToDateOptions = 'yyyy-MM-dd HH:mm'): string {
-    value = toDate(value, options);
+  transform(value: Date | string | number, formatString = 'yyyy-MM-dd HH:mm'): string {
+    value = toDate(value);
     if (isNaN(value as NzSafeAny)) return '';
 
-    const formatString = typeof options === 'string' ? options : options.formatString!;
     const langOpt = { locale: this.nzI18n.getDateLocale() };
     return formatString === 'fn' ? formatDistanceToNow(value, langOpt) : format(value, formatString, langOpt);
   }
