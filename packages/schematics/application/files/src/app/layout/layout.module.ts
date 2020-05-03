fbc09d188c0b9b8ab496534881141224b9454eb5
卡色 <cipchk@qq.com>
2020-05-03 18:45:54 +0800

    fix(cli:ng-add): fix lose component import (#873)

--- packages/schematics/application/files/src/app/layout/layout.module.ts
+++ packages/schematics/application/files/src/app/layout/layout.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared';
 import { LayoutDefaultComponent } from './default/default.component';
-import { HeaderFullScreenComponent } from './default/header/components/fullscreen.component';
+import { HeaderFullScreenComponent } from './default/header/components/fullscreen.component';<% if (i18n) { %>
+import { HeaderI18nComponent } from './default/header/components/i18n.component';<% } %>
 import { HeaderIconComponent } from './default/header/components/icon.component';
 import { HeaderNotifyComponent } from './default/header/components/notify.component';
 import { HeaderSearchComponent } from './default/header/components/search.component';
