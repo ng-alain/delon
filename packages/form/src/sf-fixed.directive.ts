fa9a9db6e3446735a6df027135d957af9c437bc6
cipchk <cipchk@qq.com>
2020-05-14 12:16:24 +0800

    fix(form): fix misalignment in fixed label
    - close https://github.com/ng-alain/ng-alain/issues/1647

--- packages/form/src/sf-fixed.directive.ts
+++ packages/form/src/sf-fixed.directive.ts
@@ -13,13 +13,12 @@ export class SFFixedDirective implements AfterViewInit, OnChanges {
     const widgetEl = this.el.querySelector('.ant-row') || this.el;
     this.render.addClass(widgetEl, 'sf__fixed');
     const labelEl = widgetEl.querySelector('.ant-form-item-label');
+    const controlEl = widgetEl.querySelector('.ant-form-item-control');
     const unit = this.num + 'px';
     if (labelEl) {
-      this.render.setStyle(labelEl, 'width', unit);
-      this.render.setStyle(labelEl, 'max-width', unit);
       this.render.setStyle(labelEl, 'flex', `0 0 ${unit}`);
+      this.render.setStyle(controlEl, 'max-width', `calc(100% - ${unit})`);
     } else {
-      const controlEl = widgetEl.querySelector('.ant-form-item-control');
       this.render.setStyle(controlEl, 'margin-left', unit);
     }
   }
