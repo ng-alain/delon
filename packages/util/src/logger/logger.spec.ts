c38573ddebb09a1907cf9e7eb1ddc3e79d9181bf
cipchk <cipchk@qq.com>
2020-06-12 20:09:32 +0800

    chore: fix

--- packages/util/src/logger/logger.spec.ts
+++ packages/util/src/logger/logger.spec.ts
@@ -1,16 +1,12 @@
-import { environment } from 'ng-zorro-antd/core/environments';
-import { warn, warnDeprecation } from './logger';
+import { warn } from './logger';
 
 describe('util.logger', () => {
-  beforeEach(() => spyOn(console, 'warn'));
+  beforeEach(() => {
+    spyOn(console, 'warn');
+  });
   it('should be once warn when same content', () => {
     warn('once');
     warn('once');
     expect(console.warn).toHaveBeenCalledTimes(1);
-  });
-  it('should be disabled warnDeprecation in prod env', () => {
-    spyOnProperty(environment, 'isTestMode', 'get').and.returnValue(false);
-    warnDeprecation('1');
-    expect(console.warn).not.toHaveBeenCalled();
   });
 });
