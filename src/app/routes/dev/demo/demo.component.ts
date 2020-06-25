ec2ad27c53c68b78054c5231953a54c894f90e22
cipchk <cipchk@qq.com>
2020-06-25 21:55:42 +0800

    feat(abc:image): add `useHttp` property
    - close https://github.com/ng-alain/ng-alain/issues/1725

--- src/app/routes/dev/demo/demo.component.ts
+++ src/app/routes/dev/demo/demo.component.ts
@@ -1,41 +1,9 @@
 import { Component } from '@angular/core';
-import { SFSchema, SFUploadWidgetSchema } from '@delon/form';
-import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
-  selector: 'app-demo',
-  template: ` <sf [schema]="schema" (formSubmit)="submit($event)"></sf> `,
+  selector: 'demo',
+  template: ` <img [_src]="src" useHttp /> `,
 })
 export class DemoComponent {
-  schema: SFSchema = {
-    properties: {
-      file: {
-        type: 'string',
-        title: '单个文件',
-        enum: [
-          {
-            uid: -1,
-            name: 'xxx.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-            response: {
-              resource_id: 1,
-            },
-          },
-        ],
-        default: 1,
-        ui: {
-          widget: 'upload',
-          action: '/upload',
-          resReName: 'resource_id',
-          urlReName: 'url',
-        } as SFUploadWidgetSchema,
-      },
-    },
-    required: ['file'],
-  };
-  constructor(public msg: NzMessageService) {}
-  submit(value: any) {
-    this.msg.success(JSON.stringify(value));
-  }
+  src = 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png';
 }
