b9aeb3b870510f4fa8f3349b5239495e149a77d4
cipchk <cipchk@qq.com>
2020-07-19 12:52:35 +0800

    chore: try remove lvy

--- packages/util/src/array/array.service.spec.ts
+++ packages/util/src/array/array.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { NzTreeBaseService, NzTreeNode } from 'ng-zorro-antd/core/tree';
 import { NzSafeAny } from 'ng-zorro-antd/core/types';
 import { AlainConfig, ALAIN_CONFIG } from '../config';
-import { deepCopy } from '../other/other';
+import { deepCopy } from '../other/deep';
 import { DelonUtilModule } from '../util.module';
 import { ArrayService } from './array.service';
 
--- scripts/ci/build-no-lvy.js
+++ scripts/ci/build-no-lvy.js
@@ -1,8 +1,10 @@
-const fs = require('fs-extra');
-const path = require('path');
+// const fs = require('fs-extra');
+// const path = require('path');
 
-const filePath = path.join(__dirname, '../../src/tsconfig.json');
+// const filePath = path.join(__dirname, '../../src/tsconfig.json');
 
-const json = fs.readJsonSync(filePath);
-json.angularCompilerOptions = { enableIvy: false };
-fs.writeJSONSync(filePath, json, { spaces: 2 });
+// const json = fs.readJsonSync(filePath);
+// json.angularCompilerOptions = { enableIvy: false };
+// fs.writeJSONSync(filePath, json, { spaces: 2 });
+
+console.log('enabled lvy');
