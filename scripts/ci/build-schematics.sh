7a21049a15ffb6cc37c1b6f758df04390c96d89d
cipchk <cipchk@qq.com>
2020-07-21 20:36:38 +0800

    release(10.0.0-beta.2): release 10.0.0-beta.2

--- scripts/ci/build-schematics.sh
+++ scripts/ci/build-schematics.sh
@@ -196,7 +196,7 @@ copyFiles() {
 cloneScaffold() {
   if [[ ! -d ng-alain ]]; then
     echo ">>> Not found scaffold source files, must be clone ng-alain ..."
-    git clone --depth 1 -b dev-ng-latest-10 https://github.com/ng-alain/ng-alain.git
+    git clone --depth 1 https://github.com/ng-alain/ng-alain.git
     echo ">>> removed .git"
     rm -rf ng-alain/.git
   else
