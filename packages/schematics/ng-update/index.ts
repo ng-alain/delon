b3468e9d237829c2c3e67c6e32d83e5ae715ac7b
cipchk <cipchk@qq.com>
2020-09-03 11:35:48 +0800

    chore: fix test of cli

--- packages/schematics/ng-update/index.ts
+++ packages/schematics/ng-update/index.ts
@@ -1,27 +1,6 @@
 import { chain, Rule } from '@angular-devkit/schematics';
-import { sync as globSync } from 'glob';
-import { delonUpgradeData } from './upgrade-data';
-import { UpgradeTSLintConfig } from './upgrade-rules/tslint-config';
 import { v10Rule } from './upgrade-rules/v10/v10Rule';
 import { v9Rule } from './upgrade-rules/v9/v9Rule';
-
-/** List of additional upgrade rules which are specifically for the CDK. */
-const extraUpgradeRules = [
-  // Misc check rules
-  'check-property-names-misc',
-];
-
-const ruleDirectories = globSync('upgrade-rules/**/', {
-  cwd: __dirname,
-  absolute: true,
-});
-
-/** TSLint upgrade configuration that will be passed to the CDK ng-update rule. */
-const tslintUpgradeConfig: UpgradeTSLintConfig = {
-  upgradeData: delonUpgradeData,
-  extraRuleDirectories: ruleDirectories,
-  extraUpgradeRules,
-};
 
 export function updateToV9(): Rule {
   return chain([v9Rule]);
