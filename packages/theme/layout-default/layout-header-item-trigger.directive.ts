9b4ae3d5f6658afb3d7e407d206585431afa5040
cipchk <cipchk@qq.com>
2020-12-07 18:00:50 +0800

    chore: update name

--- packages/theme/layout-default/layout-header-item-trigger.directive.ts
+++ packages/theme/layout-default/layout-header-item-trigger.directive.ts
@@ -1,7 +1,7 @@
 import { Directive } from '@angular/core';
 
 @Directive({
-  selector: '[layout-header-item-trigger]',
+  selector: '[layout-default-header-item-trigger]',
   host: {
     '[class.alain-default__nav-item]': `true`,
   },
