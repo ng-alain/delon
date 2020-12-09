ceb5f7c3fc6d2a6c4a41640ee1f1dda53a41f3eb
Benzara <benzara.tahar@gmail.com>
2020-12-09 21:25:45 +0100

    feat(module:theme): adding French locale

--- packages/theme/src/locale/public_api.ts
+++ packages/theme/src/locale/public_api.ts
@@ -13,3 +13,4 @@ export { default as ko_KR } from './languages/ko-KR';
 export { default as hr_HR } from './languages/hr-HR';
 export { default as ja_JP } from './languages/ja-JP';
 export { default as sl_SI } from './languages/sl-SI';
+export { default as fr_FR } from './languages/fr-FR';
