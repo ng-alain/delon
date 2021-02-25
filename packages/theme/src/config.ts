9e9cf1136d5325d2c6b2b6612cfcdab43555e7f1
cipchk <cipchk@qq.com>
2021-02-25 14:32:11 +0800

    chore: fix

--- packages/theme/src/config.ts
+++ packages/theme/src/config.ts
@@ -25,15 +25,15 @@ export interface ApiConfig {
    */
   baseUrl: string;
   /**
-   * Whether to enable automatic refresh token, default: `true`
+   * Whether to enable automatic refresh token
    *
-   * 是否启用自动刷新Token，默认：`true`
+   * 是否启用自动刷新Token
    */
   refreshTokenEnabled: boolean;
   /**
-   * Token refresh type, `re-request` Trigger token refresh request when the detection time expires, and then re-send the original request, `auth-refresh` uses `@delon/auth` to periodically check whether it has expired, default: `auth-refresh`
+   * Token refresh type, `re-request` Trigger token refresh request when the detection time expires, and then re-send the original request, `auth-refresh` uses `@delon/auth` to periodically check whether it has expired
    *
-   * 刷新Token方式，`re-request` 当检测过期时间到期时先发起刷新Token请求，再重新发起原请求，`auth-refresh` 利用 `@delon/auth` 来定期检测是否过期，默认：`auth-refresh1`
+   * 刷新Token方式，`re-request` 当检测过期时间到期时先发起刷新Token请求，再重新发起原请求，`auth-refresh` 利用 `@delon/auth` 来定期检测是否过期
    */
   refreshTokenType: 're-request' | 'auth-refresh';
 }
