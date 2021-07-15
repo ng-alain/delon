cd4e98d660572a5dedea7a68870a29c0cfba72bb
卡色 <cipchk@qq.com>
2021-07-15 18:34:10 +0800

    chore: remove simplemde (#1278)

--- src/app/shared/json-schema/json-schema.module.ts
+++ src/app/shared/json-schema/json-schema.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { DelonFormModule, WidgetRegistry } from '@delon/form';
 
-import { MarkdownWidget } from '../../../../packages/form/widgets-third/markdown/markdown.widget';
 import { TinymceWidget } from '../../../../packages/form/widgets-third/tinymce/tinymce.widget';
 import { UeditorWidget } from '../../../../packages/form/widgets-third/ueditor/ueditor.widget';
 import { SharedModule } from '../shared.module';
 
-export const SCHEMA_THIRDS_COMPONENTS = [TinymceWidget, UeditorWidget, MarkdownWidget];
+export const SCHEMA_THIRDS_COMPONENTS = [TinymceWidget, UeditorWidget];
 
 @NgModule({
   declarations: SCHEMA_THIRDS_COMPONENTS,
@@ -19,6 +18,5 @@ export class JsonSchemaModule {
   constructor(widgetRegistry: WidgetRegistry) {
     widgetRegistry.register(TinymceWidget.KEY, TinymceWidget);
     widgetRegistry.register(UeditorWidget.KEY, UeditorWidget);
-    widgetRegistry.register(MarkdownWidget.KEY, MarkdownWidget);
   }
 }
