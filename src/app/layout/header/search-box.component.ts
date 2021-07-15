fd2813a04de2441948247a0bed7321d253bc0d26
cipchk <cipchk@qq.com>
2021-07-15 17:47:11 +0800

    chore: use cdn in docsearch

--- src/app/layout/header/search-box.component.ts
+++ src/app/layout/header/search-box.component.ts
@@ -7,6 +7,7 @@ import { I18NService } from '@core';
 import { NzSafeAny } from 'ng-zorro-antd/core/types';
 
 import { ALAIN_I18N_TOKEN } from '@delon/theme';
+import { LazyService } from '@delon/util/other';
 
 declare const docsearch: any;
 
@@ -25,7 +26,8 @@ export class HeaderSearchComponent implements AfterViewInit {
   constructor(
     @Inject(ALAIN_I18N_TOKEN) private i18n: I18NService,
     private platform: Platform,
-    private router: Router
+    private router: Router,
+    private lazySrv: LazyService
   ) {}
 
   ngAfterViewInit(): void {
@@ -37,27 +39,30 @@ export class HeaderSearchComponent implements AfterViewInit {
       return;
     }
 
-    const curHost = location.hostname;
-    const isLocal = curHost.includes('localhost');
-    docsearch({
-      // appId: '2WSH9IUML3',
-      apiKey: 'abc8efef8b964f6ab0629f0ded98ab29',
-      indexName: 'ng-alain',
-      inputSelector: '#search-box input',
-      algoliaOptions: {
-        hitsPerPage: 5,
-        facetFilters: [`tags:${this.i18n.zone}`]
-      },
-      handleSelected: (_input: NzSafeAny, _event: NzSafeAny, suggestion: { url: string }) => {
-        const url = suggestion?.url || '';
-        if (isLocal || curHost === this.getHost(url)) {
-          const pathName = url.replace(/.*\/\/[^\/]*/, '');
-          this.router.navigateByUrl(pathName);
-          return;
-        }
-        window.open(url);
-      },
-      debug: false
+    const start = `https://cdnjs.cloudflare.com/ajax/libs/docsearch.js/2.6.3/docsearch.min`;
+    this.lazySrv.load([`${start}.js`, `${start}.css`]).then(() => {
+      const curHost = location.hostname;
+      const isLocal = curHost.includes('localhost');
+      docsearch({
+        // appId: '2WSH9IUML3',
+        apiKey: 'abc8efef8b964f6ab0629f0ded98ab29',
+        indexName: 'ng-alain',
+        inputSelector: '#search-box input',
+        algoliaOptions: {
+          hitsPerPage: 5,
+          facetFilters: [`tags:${this.i18n.zone}`]
+        },
+        handleSelected: (_input: NzSafeAny, _event: NzSafeAny, suggestion: { url: string }) => {
+          const url = suggestion?.url || '';
+          if (isLocal || curHost === this.getHost(url)) {
+            const pathName = url.replace(/.*\/\/[^\/]*/, '');
+            this.router.navigateByUrl(pathName);
+            return;
+          }
+          window.open(url);
+        },
+        debug: false
+      });
     });
   }
 
