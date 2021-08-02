a134faf26e8387088869ea3bb13d45374e26a65c
cipchk <cipchk@qq.com>
2021-08-02 19:10:07 +0800

    chore: fix

--- packages/abc/st/st-export.ts
+++ packages/abc/st/st-export.ts
@@ -1,6 +1,6 @@
 import { Injectable, Optional } from '@angular/core';
 
-import type { XlsxExportResult, XlsxService } from '@delon/abc/xlsx';
+import { XlsxExportResult, XlsxService } from '@delon/abc/xlsx';
 import { deepGet } from '@delon/util/other';
 import type { NzSafeAny } from 'ng-zorro-antd/core/types';
 
