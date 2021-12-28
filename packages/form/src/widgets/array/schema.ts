7b8913e4c603dda6e2883575d8943cef66672a62
cipchk <cipchk@qq.com>
2021-12-28 12:31:50 +0800

    fix(form:array): add `required` property
    - close https://github.com/ng-alain/ng-alain/issues/2166

--- packages/form/src/widgets/array/schema.ts
+++ packages/form/src/widgets/array/schema.ts
@@ -4,21 +4,52 @@ import { FormProperty } from '../../model/form.property';
 import { SFUISchemaItem } from '../../schema/ui';
 
 export interface SFArrayWidgetSchema extends SFUISchemaItem {
-  /** 指定添加按钮文本，默认：添加 */
+  /**
+   * Add button title, default: `添加`
+   *
+   * 指定添加按钮文本，默认：`添加`
+   */
   addTitle?: string;
 
-  /** 指定添加按钮风格，等同按钮 `nzType`，默认：dashed */
+  /**
+   * Add button type, equals to `nzType`, default: `dashed`
+   *
+   * 指定添加按钮风格，等同按钮 `nzType`，默认：`dashed`
+   */
   addType?: NzButtonType;
 
-  /** 指定是否显示移除按钮 */
+  /**
+   * Whether includes remove button, default: `true`
+   *
+   * 指定是否显示移除按钮，默认：`true`
+   */
   removable?: boolean;
 
-  /** 指定移除按钮文本，默认：移除 */
+  /**
+   * Remove button title, default: `移除`
+   *
+   * 指定移除按钮文本，默认：`移除`
+   */
   removeTitle?: string;
 
-  /** 添加回调，`property` 表示添加后的表单属性 */
+  /**
+   * Add required style to current item
+   *
+   * 当前项是否为必填，仅影响样式
+   */
+  required?: boolean;
+
+  /**
+   * Add callback，`property` indicates form property after add
+   *
+   * 添加回调，`property` 表示添加后的表单属性
+   */
   add: (property: FormProperty) => void;
 
-  /** 移除回调 */
+  /**
+   * Remove callback
+   *
+   * 移除回调
+   */
   remove: (index: number) => void;
 }
