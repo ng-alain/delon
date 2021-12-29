3cd4bbaa484d60f1a20681ae5f067923c0703b49
cipchk <cipchk@qq.com>
2021-12-29 15:38:02 +0800

    perf(util:copy): use standard interface `navigator.clipboard`

--- packages/util/browser/copy.ts
+++ packages/util/browser/copy.ts
@@ -4,22 +4,5 @@
  * 复制字符串文档至剪贴板
  */
 export function copy(value: string): Promise<string> {
-  return new Promise<string>((resolve): void => {
-    let copyTextArea: HTMLTextAreaElement | null = null;
-    try {
-      copyTextArea = document.createElement('textarea');
-      copyTextArea.style.height = '0px';
-      copyTextArea.style.opacity = '0';
-      copyTextArea.style.width = '0px';
-      document.body.appendChild(copyTextArea);
-      copyTextArea.value = value;
-      copyTextArea.select();
-      document.execCommand('copy');
-      resolve(value);
-    } finally {
-      if (copyTextArea && copyTextArea.parentNode) {
-        copyTextArea.parentNode.removeChild(copyTextArea);
-      }
-    }
-  });
+  return navigator.clipboard.writeText(value).then(() => Promise.resolve(value));
 }
