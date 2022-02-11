95361e684e9810be3136795e950f1b3cb1be44be
卡色 <cipchk@qq.com>
2022-02-11 16:03:14 +0800

    chore: fix algolia (#1434)

--- src/app/layout/header/search-box.component.ts
+++ src/app/layout/header/search-box.component.ts
@@ -38,17 +38,19 @@ export class HeaderSearchComponent implements AfterViewInit {
       return;
     }
 
+    // Admin panel: https://crawler.algolia.com/admin/
     const start = `https://cdnjs.cloudflare.com/ajax/libs/docsearch.js/2.6.3/docsearch.min`;
     this.lazySrv.load([`${start}.js`, `${start}.css`]).then(() => {
       const curHost = location.hostname;
       const isLocal = curHost.includes('localhost');
       docsearch({
-        // appId: '2WSH9IUML3',
-        apiKey: 'abc8efef8b964f6ab0629f0ded98ab29',
-        indexName: 'ng-alain',
+        appId: 'KNI4PDQPLT',
+        apiKey: 'f77b841e2a1f87742bef959fe09cc80e',
+        indexName: `ng-alain`,
         inputSelector: '#search-box input',
         algoliaOptions: {
           hitsPerPage: 5,
+          // queryLanguages: [this.i18n.zone]
           facetFilters: [`tags:${this.i18n.zone}`]
         },
         handleSelected: (_input: NzSafeAny, _event: NzSafeAny, suggestion: { url: string }) => {
