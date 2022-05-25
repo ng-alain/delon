c5c6df0bbd10ba75a69ea781762d831ccbf57990
cipchk <cipchk@qq.com>
2022-05-25 19:57:05 +0800

    fix(util:copy): fix invalid `copy` in latest chrome
    - close https://github.com/ng-alain/ng-alain/issues/2246

--- packages/util/browser/copy.spec.ts
+++ packages/util/browser/copy.spec.ts
@@ -1,11 +1,31 @@
+import type { NzSafeAny } from 'ng-zorro-antd/core/types';
+
 import { copy } from './copy';
 
 describe('abc: utils', () => {
   describe('#copy', () => {
-    it('should be copy a string', () => {
-      const writeTextSpy = spyOn(navigator.clipboard, 'writeText');
-      copy('test');
-      expect(writeTextSpy).toHaveBeenCalled();
+    it('should be copy a string', (done: () => void) => {
+      copy('test')
+        .then(() => {
+          expect(true).toBe(true);
+          done();
+        })
+        .catch(() => {
+          expect(false).toBe(true);
+          done();
+        });
+    });
+    it('[[boundary]]', (done: () => void) => {
+      spyOn(document, 'createElement').and.returnValue({ parentNode: null } as NzSafeAny);
+      copy('test')
+        .then(() => {
+          expect(false).toBe(true);
+          done();
+        })
+        .catch(() => {
+          expect(true).toBe(true);
+          done();
+        });
     });
   });
 });
--- packages/util/browser/copy.ts
+++ packages/util/browser/copy.ts
@@ -3,6 +3,24 @@
  *
  * 复制字符串文档至剪贴板
  */
-export function copy(value: string): Promise<void> {
-  return navigator.clipboard.writeText(value);
+export function copy(value: string): Promise<string> {
+  return new Promise<string>((resolve): void => {
+    let copyTextArea: HTMLTextAreaElement | null = null;
+    try {
+      copyTextArea = document.createElement('textarea');
+      copyTextArea.style.height = '0px';
+      copyTextArea.style.opacity = '0';
+      copyTextArea.style.width = '0px';
+      document.body.appendChild(copyTextArea);
+      copyTextArea.value = value;
+      copyTextArea.select();
+      // eslint-disable-next-line deprecation/deprecation
+      document.execCommand('copy');
+      resolve(value);
+    } finally {
+      if (copyTextArea && copyTextArea.parentNode) {
+        copyTextArea.parentNode.removeChild(copyTextArea);
+      }
+    }
+  });
 }
