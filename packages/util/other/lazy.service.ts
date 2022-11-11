a197bb1453b72b5645878dedd55f17d3436f93ae
卡色 <cipchk@qq.com>
2022-11-11 19:11:24 +0800

    feat(util:other:lazy): add `attriburte` property (#1548)

--- packages/util/other/lazy.service.ts
+++ packages/util/other/lazy.service.ts
@@ -10,7 +10,20 @@ export interface LazyResult {
   error?: NzSafeAny;
 }
 
+export interface LazyLoadItem {
+  path: string;
+  options?: LazyLoadOptions;
+}
+
+export interface LazyLoadOptions {
+  innerContent?: string;
+  attributes?: { [qualifiedName: string]: string };
+  rel?: string;
+}
+
 /**
+ * `LazyService` delay loading JS or CSS files.
+ *
  * 延迟加载资源（js 或 css）服务
  */
 @Injectable({ providedIn: 'root' })
@@ -33,19 +46,32 @@ export class LazyService {
     this.cached = {};
   }
 
-  load(paths: string | string[]): Promise<LazyResult[]> {
+  private attachAttributes(el: HTMLElement, attributes?: { [qualifiedName: string]: string }): void {
+    if (attributes == null) return;
+
+    Object.entries(attributes).forEach(([key, value]) => {
+      el.setAttribute(key, value);
+    });
+  }
+
+  /**
+   * Load script or style files
+   */
+  load(paths: string | LazyLoadItem | Array<string | LazyLoadItem>): Promise<LazyResult[]> {
     if (!Array.isArray(paths)) {
       paths = [paths];
     }
 
     const promises: Array<Promise<LazyResult>> = [];
-    paths.forEach(path => {
-      if (path.endsWith('.js')) {
-        promises.push(this.loadScript(path));
-      } else {
-        promises.push(this.loadStyle(path));
-      }
-    });
+    paths
+      .map(v => (typeof v !== 'object' ? ({ path: v } as LazyLoadItem) : v))
+      .forEach(item => {
+        if (item.path.endsWith('.js')) {
+          promises.push(this.loadScript(item.path, item.options));
+        } else {
+          promises.push(this.loadStyle(item.path, item.options));
+        }
+      });
 
     return Promise.all(promises).then(res => {
       this._notify.next(res);
@@ -53,7 +79,26 @@ export class LazyService {
     });
   }
 
-  loadScript(path: string, innerContent?: string): Promise<LazyResult> {
+  /**
+   * @deprecated Will be removed in 15.0.0, Please use `loadScript(path, options)` instead
+   */
+  loadScript(path: string, innerContent: string, attributes?: { [qualifiedName: string]: string }): Promise<LazyResult>;
+  /**
+   * Load a script file
+   */
+  loadScript(path: string, options?: LazyLoadOptions): Promise<LazyResult>;
+  loadScript(
+    path: string,
+    innerContent?: string | LazyLoadOptions,
+    attributes?: { [qualifiedName: string]: string }
+  ): Promise<LazyResult> {
+    const options: LazyLoadOptions =
+      typeof innerContent === 'object'
+        ? innerContent
+        : {
+            innerContent,
+            attributes
+          };
     return new Promise(resolve => {
       if (this.list[path] === true) {
         resolve({ ...this.cached[path], status: 'loading' });
@@ -70,8 +115,9 @@ export class LazyService {
       const node = this.doc.createElement('script') as HTMLScriptElement;
       node.type = 'text/javascript';
       node.src = path;
-      if (innerContent) {
-        node.innerHTML = innerContent;
+      this.attachAttributes(node, options.attributes);
+      if (options.innerContent) {
+        node.innerHTML = options.innerContent;
       }
       node.onload = () =>
         onSuccess({
@@ -88,7 +134,33 @@ export class LazyService {
     });
   }
 
-  loadStyle(path: string, rel: string = 'stylesheet', innerContent?: string): Promise<LazyResult> {
+  /**
+   * @deprecated Will be removed in 15.0.0, Please use `loadStyle(path, options)` instead
+   */
+  loadStyle(
+    path: string,
+    rel: string,
+    innerContent?: string,
+    attributes?: { [qualifiedName: string]: string }
+  ): Promise<LazyResult>;
+  /**
+   * Load a style file
+   */
+  loadStyle(path: string, options?: LazyLoadOptions): Promise<LazyResult>;
+  loadStyle(
+    path: string,
+    rel?: string | LazyLoadOptions,
+    innerContent?: string,
+    attributes?: { [qualifiedName: string]: string }
+  ): Promise<LazyResult> {
+    const options: LazyLoadOptions =
+      typeof rel === 'object'
+        ? rel
+        : {
+            rel,
+            innerContent,
+            attributes
+          };
     return new Promise(resolve => {
       if (this.list[path] === true) {
         resolve(this.cached[path]);
@@ -98,11 +170,12 @@ export class LazyService {
       this.list[path] = true;
 
       const node = this.doc.createElement('link') as HTMLLinkElement;
-      node.rel = rel;
+      node.rel = options.rel ?? 'stylesheet';
       node.type = 'text/css';
       node.href = path;
-      if (innerContent) {
-        node.innerHTML = innerContent;
+      this.attachAttributes(node, options.attributes);
+      if (options.innerContent) {
+        node.innerHTML = options.innerContent;
       }
       this.doc.getElementsByTagName('head')[0].appendChild(node);
       const item: LazyResult = {
