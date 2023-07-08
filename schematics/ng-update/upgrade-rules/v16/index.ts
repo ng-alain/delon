2ecc28b53773d9b5215ebd720be4ead55d78c705
卡色 <cipchk@qq.com>
2023-07-08 14:24:19 +0800

    fix(cli): remove `stylelint-config-prettier` (#1606)

--- schematics/ng-update/upgrade-rules/v16/index.ts
+++ schematics/ng-update/upgrade-rules/v16/index.ts
@@ -2,8 +2,34 @@ import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics'
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import * as colors from 'ansi-colors';
 
-import { logStart } from '../../../utils';
+import { logStart, readJSON, readPackage, writeJSON, writePackage } from '../../../utils';
 import { UpgradeMainVersions } from '../../../utils/versions';
+
+function removeStylelintConfigPrettier(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const key = `stylelint-config-prettier`;
+    context.logger.info(colors.yellow(` ${key} have been deprecated`));
+    // remove package.json
+    const packageJson = readPackage(tree);
+    if (packageJson != null) {
+      delete packageJson.devDependencies[key];
+      writePackage(tree, packageJson);
+    }
+
+    // remote .stylelintrc
+    const stylelintrcFilePath = '.stylelintrc';
+    const stylelintrcjson = readJSON(tree, stylelintrcFilePath);
+    if (stylelintrcjson != null) {
+      if (!Array.isArray(stylelintrcjson.extends)) stylelintrcjson.extends = [];
+      const arr: string[] = stylelintrcjson.extends;
+      const idx = arr.indexOf(key);
+      if (idx !== -1) {
+        arr.splice(idx, 1);
+        writeJSON(tree, stylelintrcFilePath, stylelintrcjson);
+      }
+    }
+  };
+}
 
 function finished(): Rule {
   return (_tree: Tree, context: SchematicContext) => {
@@ -21,6 +47,6 @@ export function v16Rule(): Rule {
   return async (tree: Tree, context: SchematicContext) => {
     logStart(context, `Upgrade @delon/* version number`);
     UpgradeMainVersions(tree);
-    return chain([finished()]);
+    return chain([removeStylelintConfigPrettier(), finished()]);
   };
 }
