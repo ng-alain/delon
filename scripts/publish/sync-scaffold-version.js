10f4712a457486433f71624d3a084a428e4d6444
cipchk <cipchk@qq.com>
2023-09-01 22:10:31 +0800

    release(16.3.0): release 16.3.0

--- scripts/publish/sync-scaffold-version.js
+++ scripts/publish/sync-scaffold-version.js
@@ -1,22 +1,27 @@
 // node scripts/publish/sync-scaffold-version.js
 // node scripts/publish/sync-scaffold-version.js theme
-const fs = require('fs-extra');
-const path = require('path');
+import { existsSync } from 'fs';
+import { readJSONSync, writeJSONSync } from 'fs-extra/esm';
+import { join, resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const nextJson = fs.readJSONSync(path.join(__dirname, '../../package.json'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextJson = readJSONSync(join(__dirname, '../../package.json'));
 const nextVersion = nextJson.version;
 const nextVersions = {
   ...nextJson.dependencies,
   ...nextJson.devDependencies,
 };
 const name = (process.argv.length >= 2 ? process.argv[2] : '') || 'ng-alain';
-const packagePath = path.resolve(__dirname, name.startsWith('../') ? name : `../../../${name}/package.json`);
+const packagePath = resolve(__dirname, name.startsWith('../') ? name : `../../../${name}/package.json`);
 console.log(`Use packagePath: ` + packagePath);
-if (!fs.existsSync(packagePath)) {
+if (!existsSync(packagePath)) {
   throw Error(`所选的文件不存在, ${packagePath}`)
 }
 
-const json = fs.readJSONSync(packagePath);
+const json = readJSONSync(packagePath);
 // Update third party
 ['dependencies', 'devDependencies'].forEach(type => {
   Object.keys(json[type])
@@ -36,6 +41,6 @@ json.devDependencies[`@delon/testing`] = `^${nextVersion}`;
 json.devDependencies[`ng-alain`] = `^${nextVersion}`;
 
 // Save
-fs.writeJSONSync(packagePath, json, {
+writeJSONSync(packagePath, json, {
   spaces: 2,
 });
