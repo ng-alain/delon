8ff053c1a2b918678a8be34ba49a6f2ee53c5f7f
cipchk <cipchk@qq.com>
2023-11-11 22:50:18 +0800

    chore: fix

--- schematics/ng-update/upgrade-rules/v17/index.spec.ts
+++ schematics/ng-update/upgrade-rules/v17/index.spec.ts
@@ -1,5 +1,6 @@
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 
+import jsonSchema from './files/jsonSchemaModule';
 import { tryAddFile } from '../../../utils';
 import { createAlainApp, migrationCollection } from '../../../utils/testing';
 
@@ -7,34 +8,9 @@ describe('Schematic: ng-update: v17Rule', () => {
   let runner: SchematicTestRunner;
   let tree: UnitTestTree;
   const logs: string[] = [];
-  const jsonSchemaModulePath = '/projects/foo/src/app/shared/json-schema/json-schema.module.ts';
 
   beforeEach(async () => {
     ({ runner, tree } = await createAlainApp());
-    if (!tree.exists(jsonSchemaModulePath)) {
-      tree.create(
-        jsonSchemaModulePath,
-        `import { NgModule } from '@angular/core';
-    import { DelonFormModule, WidgetRegistry } from '@delon/form';
-
-    import { TestWidget } from './test/test.widget';
-    import { SharedModule } from '../shared.module';
-
-    export const SCHEMA_THIRDS_COMPONENTS = [TestWidget];
-
-    @NgModule({
-      declarations: SCHEMA_THIRDS_COMPONENTS,
-      imports: [SharedModule, DelonFormModule.forRoot()],
-      exports: SCHEMA_THIRDS_COMPONENTS
-    })
-    export class JsonSchemaModule {
-      constructor(widgetRegistry: WidgetRegistry) {
-        widgetRegistry.register(TestWidget.KEY, TestWidget);
-      }
-    }
-    `
-      );
-    }
   });
 
   async function runMigration(): Promise<void> {
@@ -45,6 +21,8 @@ describe('Schematic: ng-update: v17Rule', () => {
   }
 
   it('should be working', async () => {
+    const jsonSchemaModulePath = '/projects/foo/src/app/shared/json-schema/json-schema.module.ts';
+    tryAddFile(tree, jsonSchemaModulePath, jsonSchema);
     await runMigration();
     const content = tree.readContent(jsonSchemaModulePath);
     expect(content).toContain(`import { UploadWidgetModule } from '@delon/form/widgets/upload';`);
