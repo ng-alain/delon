358cebf4b9fe50e8d7ca48924151f114a502ad07
cipchk <cipchk@qq.com>
2023-11-22 02:31:32 +0800

    chore: perf mock interceptor

--- packages/mock/src/provide.ts
+++ packages/mock/src/provide.ts
@@ -1,20 +1,9 @@
-import { ɵHTTP_ROOT_INTERCEPTOR_FNS } from '@angular/common/http';
 import { EnvironmentProviders, InjectionToken, makeEnvironmentProviders } from '@angular/core';
 
 import { MockOptions } from './interface';
-import { mockInterceptor } from './mock.interceptor';
-import { MockService } from './mock.service';
 
 export const DELON_MOCK_CONFIG = new InjectionToken<MockOptions>('alain-mock-config');
 
 export function provideMockConfig(config?: MockOptions): EnvironmentProviders {
-  return makeEnvironmentProviders([
-    { provide: DELON_MOCK_CONFIG, useValue: config },
-    {
-      provide: ɵHTTP_ROOT_INTERCEPTOR_FNS,
-      useValue: mockInterceptor,
-      multi: true,
-      deps: [MockService]
-    }
-  ]);
+  return makeEnvironmentProviders([{ provide: DELON_MOCK_CONFIG, useValue: config }]);
 }
