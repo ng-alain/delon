7045cec1bb78d114cb606e2295597ac8263b6b22
卡色 <cipchk@qq.com>
2023-11-24 12:18:02 +0800

    chore: type of widget registry items (#1715)

--- packages/abc/st/provide.ts
+++ packages/abc/st/provide.ts
@@ -4,10 +4,15 @@ import type { NzSafeAny } from 'ng-zorro-antd/core/types';
 
 import { STWidgetRegistry } from './st-widget';
 
+export interface STWidgetProvideConfig {
+  KEY: string;
+  type: NzSafeAny;
+}
+
 /**
  * Just only using Standalone widgets
  */
-export function provideSTWidgets(...widgets: Array<{ KEY: string; type: NzSafeAny }>): EnvironmentProviders {
+export function provideSTWidgets(...widgets: STWidgetProvideConfig[]): EnvironmentProviders {
   return makeEnvironmentProviders([
     {
       provide: ENVIRONMENT_INITIALIZER,
