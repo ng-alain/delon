33fe34e33051257ffa278470ae54e59493a017a0
cipchk <cipchk@qq.com>
2023-11-25 15:54:22 +0800

    chore: fix build

--- scripts/ci/build-schematics.sh
+++ scripts/ci/build-schematics.sh
@@ -148,7 +148,7 @@ copyFiles() {
 cloneScaffold() {
   if [[ ! -d ng-alain ]]; then
     echo ">>> Not found scaffold source files, must be clone ng-alain ..."
-    git clone --depth 1 https://github.com/ng-alain/ng-alain.git -b feat-config
+    git clone --depth 1 https://github.com/ng-alain/ng-alain.git
     echo ">>> removed .git"
     rm -rf ng-alain/.git
   else
