d1d3a5188f3bc56ee362a35b158ac8de9a08721e
cipchk <cipchk@users.noreply.github.com>
2023-11-26 06:43:45 +0000

    deploy: d5890d8af9176a02c9dcfffe18fd915f878b8526

---
+++ chunk-2HATZH5X.js
@@ -0,0 +1,17 @@
+import{Ab as u,Db as f,Ja as r,Jb as v,Ka as s,Kb as x,Sa as c,ag as y,cg as h,eg as D,fb as i,gb as n,gg as C,ha as d,kg as _,pb as p,qc as z,rg as k,yb as e}from"./chunk-SBXYWWID.js";var F=(()=>{let t=class t{constructor(m){this.modal=m}ok(){this.modal.destroy(`new time: ${+new Date}`)}cancel(){this.modal.destroy()}};t.\u0275fac=function(o){return new(o||t)(s(_))},t.\u0275cmp=d({type:t,selectors:[["app-demo-dialog-modal"]],inputs:{record:"record"},standalone:!0,features:[f],decls:20,vars:7,consts:[[1,"modal-header"],[1,"modal-title"],[1,"modal-footer"],["nz-button","",3,"nzType","nzSize","click"]],template:function(o,l){o&1&&(e(0,`
+    `),i(1,"div",0),e(2,`
+      `),i(3,"div",1),e(4,"Custom component"),n(),e(5,`
+    `),n(),e(6,`
+    `),i(7,"p"),e(8),v(9,"json"),n(),e(10,`
+    `),i(11,"div",2),e(12,`
+      `),i(13,"button",3),p("click",function(){return l.cancel()}),e(14," Cancel "),n(),e(15,`
+      `),i(16,"button",3),p("click",function(){return l.ok()}),e(17," OK "),n(),e(18,`
+    `),n(),e(19,`
+  `)),o&2&&(r(8),u("\u53C2\u6570\uFF1A",x(9,5,l.record),""),r(5),c("nzType","default")("nzSize","large"),r(3),c("nzType","primary")("nzSize","large"))},dependencies:[C,D,y,h,z],encapsulation:2});let a=t;return a})();var R=(()=>{let t=class t{constructor(m){this.ref=m}ok(){this.ref.close(`new time: ${+new Date}`),this.cancel()}cancel(){this.ref.close()}};t.\u0275fac=function(o){return new(o||t)(s(k))},t.\u0275cmp=d({type:t,selectors:[["app-demo-dialog-drawer"]],inputs:{record:"record"},standalone:!0,features:[f],decls:16,vars:5,consts:[[2,"height","1000px"],["routerLink","/"],[1,"drawer-footer"],["nz-button","",3,"nzType","click"]],template:function(o,l){o&1&&(e(0,`
+    `),i(1,"p",0),e(2),v(3,"json"),i(4,"a",1),e(5,"Change Router"),n()(),e(6,`
+    The end!
+    `),i(7,"div",2),e(8,`
+      `),i(9,"button",3),p("click",function(){return l.cancel()}),e(10," Cancel "),n(),e(11,`
+      `),i(12,"button",3),p("click",function(){return l.ok()}),e(13," OK "),n(),e(14,`
+    `),n(),e(15,`
+  `)),o&2&&(r(2),u("\u53C2\u6570\uFF1A",x(3,3,l.record),","),r(7),c("nzType","default"),r(3),c("nzType","primary"))},dependencies:[C,D,y,h,z],encapsulation:2});let a=t;return a})();export{F as a,R as b};
