36b922202f184752b6c20442e5b9c322e44180ae
cipchk <cipchk@qq.com>
2024-01-11 19:41:31 +0800

    fix: fix test

--- packages/abc/cell/cell.component.ts
+++ packages/abc/cell/cell.component.ts
@@ -26,7 +26,7 @@ import { NzBadgeComponent } from 'ng-zorro-antd/badge';
 import { NzCheckboxComponent } from 'ng-zorro-antd/checkbox';
 import type { NzSafeAny } from 'ng-zorro-antd/core/types';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
-import { NzImage, NzImageService } from 'ng-zorro-antd/image';
+import { NzImage, NzImageModule, NzImageService } from 'ng-zorro-antd/image';
 import { NzRadioComponent } from 'ng-zorro-antd/radio';
 import { NzTagComponent } from 'ng-zorro-antd/tag';
 import { NzTooltipDirective } from 'ng-zorro-antd/tooltip';
@@ -121,6 +121,7 @@ import type { CellDefaultText, CellOptions, CellTextResult, CellValue, CellWidge
     NzTagComponent,
     NzBadgeComponent,
     NzTooltipDirective,
+    NzImageModule,
     CellHostDirective
   ]
 })
