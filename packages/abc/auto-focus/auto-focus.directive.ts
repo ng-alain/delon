7c09b6ada0f07f93d9b72963b6d846afb939cc20
cipchk <cipchk@qq.com>
2024-01-18 17:43:07 +0800

    perf(abc:auto-focus): perf code

--- packages/abc/auto-focus/auto-focus.directive.ts
+++ packages/abc/auto-focus/auto-focus.directive.ts
@@ -1,43 +1,34 @@
 import { Platform } from '@angular/cdk/platform';
-import { AfterViewInit, ChangeDetectorRef, Directive, ElementRef, Input, OnDestroy } from '@angular/core';
+import { AfterViewInit, DestroyRef, Directive, ElementRef, Input, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { timer } from 'rxjs';
 
-import { BooleanInput, InputBoolean, InputNumber, NumberInput } from '@delon/util/decorator';
-import type { NzSafeAny } from 'ng-zorro-antd/core/types';
+import { BooleanInput, InputBoolean, InputNumber, NumberInput } from '@delon/util/decorator/convert';
 
 @Directive({
   selector: '[auto-focus], input[autofocus="autofocus"], textarea[autofocus="autofocus"]',
   exportAs: 'autoFocus',
   standalone: true
 })
-export class AutoFocusDirective implements AfterViewInit, OnDestroy {
+export class AutoFocusDirective implements AfterViewInit {
   static ngAcceptInputType_enabled: BooleanInput;
   static ngAcceptInputType_delay: NumberInput;
+  private readonly el = inject<ElementRef<HTMLElement>>(ElementRef).nativeElement;
+  private readonly platform = inject(Platform);
+  private readonly d$ = inject(DestroyRef);
 
-  private _focusoutTimeout: NzSafeAny;
   @Input() @InputBoolean() enabled = true;
   @Input() @InputNumber() delay = 300;
 
-  constructor(
-    private el: ElementRef<HTMLElement>,
-    private cdr: ChangeDetectorRef,
-    private platform: Platform
-  ) {}
-
   ngAfterViewInit(): void {
-    const el = this.el.nativeElement;
+    const el = this.el;
     if (!this.platform.isBrowser || !(el instanceof HTMLElement) || !this.enabled) {
       return;
     }
-    this._focusoutTimeout = setTimeout(() => {
-      el.focus({ preventScroll: false });
-      this.cdr.markForCheck();
-    }, this.delay);
-  }
-
-  ngOnDestroy(): void {
-    if (this._focusoutTimeout) {
-      clearTimeout(this._focusoutTimeout);
-      this._focusoutTimeout = null;
-    }
+    timer(this.delay)
+      .pipe(takeUntilDestroyed(this.d$))
+      .subscribe(() => {
+        el.focus({ preventScroll: false });
+      });
   }
 }
