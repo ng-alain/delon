7ec51ffb4c5e634d594c5143ea47f2a1c6c16e5e
cipchk <cipchk@qq.com>
2024-01-18 18:05:28 +0800

    chore: fix code

--- packages/abc/auto-focus/auto-focus.directive.ts
+++ packages/abc/auto-focus/auto-focus.directive.ts
@@ -1,9 +1,16 @@
 import { Platform } from '@angular/cdk/platform';
-import { AfterViewInit, DestroyRef, Directive, ElementRef, Input, inject } from '@angular/core';
+import {
+  AfterViewInit,
+  DestroyRef,
+  Directive,
+  ElementRef,
+  Input,
+  booleanAttribute,
+  inject,
+  numberAttribute
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { timer } from 'rxjs';
-
-import { BooleanInput, InputBoolean, InputNumber, NumberInput } from '@delon/util/decorator/convert';
 
 @Directive({
   selector: '[auto-focus], input[autofocus="autofocus"], textarea[autofocus="autofocus"]',
@@ -11,14 +18,12 @@ import { BooleanInput, InputBoolean, InputNumber, NumberInput } from '@delon/uti
   standalone: true
 })
 export class AutoFocusDirective implements AfterViewInit {
-  static ngAcceptInputType_enabled: BooleanInput;
-  static ngAcceptInputType_delay: NumberInput;
   private readonly el = inject<ElementRef<HTMLElement>>(ElementRef).nativeElement;
   private readonly platform = inject(Platform);
   private readonly d$ = inject(DestroyRef);
 
-  @Input() @InputBoolean() enabled = true;
-  @Input() @InputNumber() delay = 300;
+  @Input({ transform: booleanAttribute }) enabled = true;
+  @Input({ transform: numberAttribute }) delay = 300;
 
   ngAfterViewInit(): void {
     const el = this.el;
@@ -27,8 +32,6 @@ export class AutoFocusDirective implements AfterViewInit {
     }
     timer(this.delay)
       .pipe(takeUntilDestroyed(this.d$))
-      .subscribe(() => {
-        el.focus({ preventScroll: false });
-      });
+      .subscribe(() => el.focus({ preventScroll: false }));
   }
 }
