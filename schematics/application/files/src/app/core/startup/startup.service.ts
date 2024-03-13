905014bd09994b784dc467ae2884a1a3e99875de
卡色 <cipchk@qq.com>
2024-03-13 15:37:53 +0800

    fix(cli): add `./` current fold in `startup.service` (#1780)

--- schematics/application/files/src/app/core/startup/startup.service.ts
+++ schematics/application/files/src/app/core/startup/startup.service.ts
@@ -35,8 +35,8 @@ export class StartupService {
   private router = inject(Router);<% if (i18n) { %>
   private i18n = inject<I18NService>(ALAIN_I18N_TOKEN);<% } %>
   // If http request allows anonymous access, you need to add `ALLOW_ANONYMOUS`:
-  // this.httpClient.get('assets/tmp/app-data.json', { context: new HttpContext().set(ALLOW_ANONYMOUS, true) })
-  private appData$ = this.httpClient.get('assets/tmp/app-data.json').pipe(
+  // this.httpClient.get('/app', { context: new HttpContext().set(ALLOW_ANONYMOUS, true) })
+  private appData$ = this.httpClient.get('./assets/tmp/app-data.json').pipe(
     catchError((res: NzSafeAny) => {
       console.warn(`StartupService.load: Network request failed`, res);
       setTimeout(() => this.router.navigateByUrl(`/exception/500`));
