5f8de39c9e60356cefa402de3ed21055fecb77e6
cipchk <cipchk@qq.com>
2025-01-18 20:51:18 +0800

    chore: fix lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -55,6 +55,7 @@ module.exports = {
     'import-notation': 'string',
     'media-feature-range-notation': 'prefix',
     'media-query-no-invalid': null,
+    'declaration-property-value-no-unknown': null,
     'order/order': [
       [
         'dollar-variables',
