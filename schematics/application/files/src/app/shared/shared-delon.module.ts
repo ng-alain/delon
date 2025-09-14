15c26e0de9aba436ae279aab40d6974b7a54abbe
devcui <devcui@outlook.com>
2025-09-14 12:47:27 +0800

    fix(schematics): remove obsolete modules (#1924)

--- schematics/application/files/src/app/shared/shared-delon.module.ts
+++ schematics/application/files/src/app/shared/shared-delon.module.ts
@@ -1,8 +1,7 @@
 import { PageHeaderModule } from '@delon/abc/page-header';
-import { ResultModule } from '@delon/abc/result';
 import { SEModule } from '@delon/abc/se';
 import { STModule } from '@delon/abc/st';
 import { SVModule } from '@delon/abc/sv';<% if (form) { %>
 import { DelonFormModule } from '@delon/form';<% } %>
 
-export const SHARED_DELON_MODULES = [PageHeaderModule, STModule, SEModule, SVModule, ResultModule<% if (form) { %>, DelonFormModule<% } %>];
+export const SHARED_DELON_MODULES = [PageHeaderModule, STModule, SEModule, SVModule<% if (form) { %>, DelonFormModule<% } %>];
