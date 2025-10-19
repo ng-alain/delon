e63d24cf18a45a004dfae12875d320350b846d76
cipchk <cipchk@users.noreply.github.com>
2025-10-19 02:49:00 +0000

    deploy: 56909b5f1bcf44e8bb06e2e153ccb6810344f93b

---
+++ chunk-QFQGBMDT.js
@@ -0,0 +1,5 @@
+import{a as T}from"./chunk-RWB7VE6G.js";import{E as b,G as h}from"./chunk-5L373TCB.js";import{$b as o,Fa as r,Ia as a,Od as z,Qa as m,Rb as t,Re as g,ha as n,kc as u,lc as f,mb as l,nb as s,ob as c,pb as d,sg as v,tg as N}from"./chunk-VMLETMXK.js";var y=class p{meta=n(T);i18n=n(z);_full;set item(i){this._full=`${this.meta.github}/edit/master/${this.i18n.get(i.urls)}`}static \u0275fac=function(e){return new(e||p)};static \u0275cmp=m({type:p,selectors:[["edit-button"]],inputs:{item:"item"},decls:7,vars:6,consts:[["target","_blank","nz-tooltip","",1,"edit-button",3,"href","nzTooltipTitle"],["nzType","edit"]],template:function(e,I){e&1&&(t(0,`
+    `),s(1,"a",0),u(2,"i18n"),t(3,`
+      `),d(4,"nz-icon",1),t(5,`
+    `),c(),t(6,`
+  `)),e&2&&(a(),l("href",o(I._full),r)("nzTooltipTitle",o(f(2,4,"app.content.edit-page"))))},dependencies:[N,v,h,b,g],encapsulation:2})};export{y as a};
