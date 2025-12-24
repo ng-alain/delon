63a2e658289077dca9a99530c6a708530389cd29
cipchk <cipchk@qq.com>
2025-12-24 21:24:16 +0800

    chore: fix

--- packages/form/widgets/upload/schema.ts
+++ packages/form/widgets/upload/schema.ts
@@ -2,7 +2,6 @@ import { Observable, Subscription } from 'rxjs';
 
 import type { SFSchemaEnumType, SFUISchemaItem } from '@delon/form';
 import type {
-  NzBeforeUploadFileType,
   NzShowUploadList,
   NzUploadChangeParam,
   NzUploadFile,
@@ -155,11 +154,6 @@ export interface SFUploadWidgetSchema extends SFUISchemaItem {
    * 点击下载文件时的回调，如果没有指定，则默认跳转到文件 url 对应的标签页
    */
   download?: (file: NzUploadFile) => void;
-
-  /**
-   * 在上传之前转换文件。支持返回一个 Observable 对象
-   */
-  transformFile?: (file: NzUploadFile) => NzBeforeUploadFileType;
 
   /**
    * 上传文件改变时的状态
