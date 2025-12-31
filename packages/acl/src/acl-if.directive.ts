114d2bcf8f0b37812461215475d9db384863ccf5
cipchk <cipchk@qq.com>
2026-01-01 00:25:05 +0800

    chore: add acl

--- packages/acl/src/acl-if.directive.ts
+++ packages/acl/src/acl-if.directive.ts
@@ -1,6 +1,15 @@
-import { Directive, EmbeddedViewRef, Input, OnDestroy, TemplateRef, ViewContainerRef, inject } from '@angular/core';
+import {
+  Directive,
+  EmbeddedViewRef,
+  TemplateRef,
+  ViewContainerRef,
+  booleanAttribute,
+  effect,
+  inject,
+  input
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Subscription, filter } from 'rxjs';
+import { filter } from 'rxjs';
 
 import { ACLService } from './acl.service';
 import { ACLCanType } from './acl.type';
@@ -9,78 +18,50 @@ import { ACLCanType } from './acl.type';
   selector: '[aclIf]',
   exportAs: 'aclIf'
 })
-export class ACLIfDirective implements OnDestroy {
+export class ACLIfDirective {
   private readonly srv = inject(ACLService);
   private readonly _viewContainer = inject(ViewContainerRef);
-  static ngAcceptInputType_except: boolean | string | undefined | null;
 
-  private _value!: ACLCanType;
-  private _change$: Subscription;
-  private _thenTemplateRef: TemplateRef<void> | null = inject(TemplateRef<void>);
-  private _elseTemplateRef: TemplateRef<void> | null = null;
   private _thenViewRef: EmbeddedViewRef<void> | null = null;
   private _elseViewRef: EmbeddedViewRef<void> | null = null;
-  private _except = false;
+
+  readonly aclIf = input.required<ACLCanType>();
+  readonly aclIfThen = input<TemplateRef<void> | null>(inject(TemplateRef<void>));
+  readonly aclIfElse = input<TemplateRef<void> | null>();
+  readonly except = input(false, { transform: booleanAttribute });
 
   constructor() {
-    this._change$ = this.srv.change
+    this.srv.change
       .pipe(
         takeUntilDestroyed(),
         filter(r => r != null)
       )
-      .subscribe(() => this._updateView());
+      .subscribe(() => this.updateView());
+
+    effect(() => this.updateView());
   }
 
-  @Input()
-  set aclIf(value: ACLCanType) {
-    this._value = value;
-    this._updateView();
-  }
-
-  @Input()
-  set aclIfThen(templateRef: TemplateRef<void> | null) {
-    this._thenTemplateRef = templateRef;
-    this._thenViewRef = null;
-    this._updateView();
-  }
-
-  @Input()
-  set aclIfElse(templateRef: TemplateRef<void> | null) {
-    this._elseTemplateRef = templateRef;
-    this._elseViewRef = null;
-    this._updateView();
-  }
-
-  @Input()
-  set except(value: boolean) {
-    this._except = value != null && `${value}` !== 'false';
-  }
-  get except(): boolean {
-    return this._except;
-  }
-
-  protected _updateView(): void {
-    const res = this.srv.can(this._value);
-    if ((res && !this.except) || (!res && this.except)) {
+  private updateView(): void {
+    const res = this.srv.can(this.aclIf());
+    const except = this.except();
+    const then = this.aclIfThen();
+    const els = this.aclIfElse();
+    if ((res && !except) || (!res && except)) {
       if (!this._thenViewRef) {
         this._viewContainer.clear();
         this._elseViewRef = null;
-        if (this._thenTemplateRef) {
-          this._thenViewRef = this._viewContainer.createEmbeddedView(this._thenTemplateRef);
+        if (then) {
+          this._thenViewRef = this._viewContainer.createEmbeddedView(then);
         }
       }
     } else {
       if (!this._elseViewRef) {
         this._viewContainer.clear();
         this._thenViewRef = null;
-        if (this._elseTemplateRef) {
-          this._elseViewRef = this._viewContainer.createEmbeddedView(this._elseTemplateRef);
+        if (els) {
+          this._elseViewRef = this._viewContainer.createEmbeddedView(els);
         }
       }
     }
-  }
-
-  ngOnDestroy(): void {
-    this._change$.unsubscribe();
   }
 }
