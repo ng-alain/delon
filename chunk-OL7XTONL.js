c52fa2161192820a93042b33e617f2f672b7af6a
cipchk <cipchk@users.noreply.github.com>
2026-02-01 08:12:50 +0000

    deploy: 762bbadd3bcef6ba66cdb97d214ac5e36d6dde0d

---
+++ chunk-OL7XTONL.js
@@ -0,0 +1,19 @@
+import{f as h,g as C}from"./chunk-UPMOSUYU.js";import"./chunk-S37YFZBA.js";import"./chunk-YIW3LUNL.js";import"./chunk-YU3HWF2I.js";import"./chunk-22CD57KE.js";import"./chunk-DL4MSDFO.js";import{Na as m,Tc as f,Wb as e,Xa as y,Yb as c,Yg as s,Zb as u,ga as l,pc as n,qc as i,sd as v,td as O,ub as I,vb as D,wb as g,xb as a,yb as d}from"./chunk-P5SMEL6S.js";var L=class r{route=l(v);first=s(new Date,"yyyy-MM-dd HH:mm:ss");now=s(new Date,"yyyy-MM-dd HH:mm:ss");ngOnInit(){console.log("LAYOUT LIST: ngOnInit")}_onReuseInit(){this.now=s(new Date,"yyyy-MM-dd HH:mm:ss"),console.log("LAYOUT LIST: by _onReuseInit",this.route.snapshot.url.toString())}ngOnDestroy(){console.log("LAYOUT LIST: ngOnDestroy")}static \u0275fac=function(t){return new(t||r)};static \u0275cmp=y({type:r,selectors:[["dev-list-layout"]],decls:18,vars:6,consts:[[1,"px-lg"]],template:function(t,o){t&1&&(e(0,`
+    `),g(1,"page-header"),e(2,`
+    `),I(3,"h1"),e(4,"Layout"),D(),e(5,`
+    `),I(6,"p"),e(7),n(8,"json"),n(9,"json"),D(),e(10,`
+    `),g(11,"hr"),e(12,`
+    `),I(13,"div",0),e(14,`
+      `),g(15,"router-outlet"),e(16,`
+    `),D(),e(17,`
+  `)),t&2&&(m(7),u("first: ",i(8,2,o.first),"\uFF0Cnow: ",i(9,4,o.now)))},dependencies:[C,h,O,f],encapsulation:2})};var x=class r{route=l(v);first=s(new Date,"yyyy-MM-dd HH:mm:ss");now=s(new Date,"yyyy-MM-dd HH:mm:ss");id=0;ngOnInit(){console.log("LIST ITEM: ngOnInit"),this.route.params.subscribe(p=>this.id=+p.id)}_onReuseInit(){this.now=s(new Date,"yyyy-MM-dd HH:mm:ss"),console.log("LIST ITEM: by _onReuseInit",this.route.snapshot.url.toString())}ngOnDestroy(){console.log("LIST ITEM: ngOnDestroy")}static \u0275fac=function(t){return new(t||r)};static \u0275cmp=y({type:r,selectors:[["dev-list-item"]],decls:14,vars:12,template:function(t,o){t&1&&(e(0,`
+    `),a(1,"h2"),e(2,"List Item"),d(),e(3,`
+    `),a(4,"p"),e(5),n(6,"json"),n(7,"json"),d(),e(8,`
+    `),a(9,"p"),e(10),n(11,"json"),d(),e(12),n(13,"json")),t&2&&(m(5),u("first: ",i(6,4,o.first),"\uFF0Cnow: ",i(7,6,o.now)),m(5),c("id: ",i(11,8,o.id)),m(2),c(`
+    page: `,i(13,10,o.route.url),`
+  `))},dependencies:[f],encapsulation:2})};var M=class r{route=l(v);first=s(new Date,"yyyy-MM-dd HH:mm:ss");now=s(new Date,"yyyy-MM-dd HH:mm:ss");id=0;ngOnInit(){console.log("LIST: ngOnInit"),this.route.params.subscribe(p=>this.id=+p.id)}_onReuseInit(){this.now=s(new Date,"yyyy-MM-dd HH:mm:ss"),console.log("LIST: by _onReuseInit",this.route.snapshot.url.toString())}ngOnDestroy(){console.log("LIST: ngOnDestroy")}static \u0275fac=function(t){return new(t||r)};static \u0275cmp=y({type:r,selectors:[["dev-list"]],decls:14,vars:12,template:function(t,o){t&1&&(e(0,`
+    `),a(1,"h2"),e(2,"List"),d(),e(3,`
+    `),a(4,"p"),e(5),n(6,"json"),n(7,"json"),d(),e(8,`
+    `),a(9,"p"),e(10),n(11,"json"),d(),e(12),n(13,"json")),t&2&&(m(5),u("first: ",i(6,4,o.first),"\uFF0Cnow: ",i(7,6,o.now)),m(5),c("id: ",i(11,8,o.id)),m(2),c(`
+    page: `,i(13,10,o.route.url),`
+  `))},dependencies:[f],encapsulation:2})};var ie=[{path:"",component:L,children:[{path:"",component:M},{path:"item",component:x}]}];export{ie as routes};
