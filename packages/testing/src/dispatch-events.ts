a1c9caff36723f9a3d6d84b8647491bf4d3b81cb
cipchk <cipchk@qq.com>
2026-02-28 16:07:14 +0800

    chore: fix build

--- packages/testing/src/dispatch-events.ts
+++ packages/testing/src/dispatch-events.ts
@@ -6,5 +6,5 @@ export function dispatchEvent(node: Node | Window, event: Event): Event {
 
 /** Shorthand to dispatch a fake event on a specified node. */
 export function dispatchFakeEvent(node: Node | Window, type: string | Event, canBubble?: boolean): Event {
-  return dispatchEvent(node, typeof type === 'string' ? new Event(type, { bubbles: canBubble }) : type);
+  return dispatchEvent(node, typeof type === 'string' ? new Event(type, { bubbles: canBubble ?? true }) : type);
 }
