9672f13240d41e826e703bf4728cb20857dd1611
cipchk <cipchk@qq.com>
2026-03-05 23:01:39 +0800

    chore: fix test

--- packages/test.ts
+++ packages/test.ts
@@ -1,6 +1,12 @@
+import { NgModule, provideZoneChangeDetection } from '@angular/core';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
-getTestBed().initTestEnvironment([BrowserTestingModule], platformBrowserTesting(), {
+@NgModule({
+  providers: [provideZoneChangeDetection()],
+})
+class AppTestingModule {}
+
+getTestBed().initTestEnvironment([BrowserTestingModule,AppTestingModule], platformBrowserTesting(), {
   teardown: { destroyAfterEach: false },
 });
