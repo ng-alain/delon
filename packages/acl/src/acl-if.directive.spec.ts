a54ed3a36a05a9f8a4b8924a655e77c2c68c71f3
marbleqi <marbleqi@163.com>
2026-03-31 11:52:21 +0800

    fix: fix acl directive tests - use fakeAsync and standalone

--- packages/acl/src/acl-if.directive.spec.ts
+++ packages/acl/src/acl-if.directive.spec.ts
@@ -1,5 +1,5 @@
 import { Component, DebugElement, inject } from '@angular/core';
-import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { ACLIfDirective } from './acl-if.directive';
@@ -15,75 +15,97 @@ describe('acl-if: directive', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [{ provide: ComponentFixtureAutoDetect, useValue: true }]
+      imports: [TestComponent]
     });
     fixture = TestBed.createComponent(TestComponent);
     dl = fixture.debugElement;
     context = dl.componentInstance;
   });
 
-  it('should show element when full', () => {
+  it('should show element when full', fakeAsync(() => {
     context.srv.setFull(true);
     context.role = 'user';
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(CLS)).length).toBe(1);
-  });
+  }));
 
-  it('should remove when not full', () => {
+  it('should remove when not full', fakeAsync(() => {
     context.srv.setFull(false);
     context.role = 'user';
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(CLS)).length).toBe(0);
-  });
+  }));
 
-  it('should switch acl', () => {
+  it('should switch acl', fakeAsync(() => {
     context.srv.setFull(false);
+    fixture.detectChanges();
+    tick();
     fixture.detectChanges();
     expect(dl.queryAll(By.css(CLS)).length).toBe(0);
     context.srv.setFull(true);
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(CLS)).length).toBe(1);
     context.srv.setFull(false);
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(CLS)).length).toBe(0);
-  });
+  }));
 
-  it('should be support complex acl value', () => {
+  it('should be support complex acl value', fakeAsync(() => {
     context.srv.setFull(false);
     context.srv.setRole(['user']);
     context.role = { role: ['user', 'manage'], mode: 'allOf' };
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(CLS)).length).toBe(0);
     context.srv.setRole(['user', 'manage']);
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(CLS)).length).toBe(1);
-  });
+  }));
 
-  it('should be show else when unauthorized', () => {
+  it('should be show else when unauthorized', fakeAsync(() => {
     context.srv.setFull(false);
     context.srv.setRole(['user']);
     context.role = 'admin';
+    fixture.detectChanges();
+    tick();
     fixture.detectChanges();
     expect(dl.queryAll(By.css(CLS)).length).toBe(0);
     expect(dl.queryAll(By.css(CLS_NOT)).length).toBe(1);
     context.role = 'user';
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(CLS)).length).toBe(1);
     expect(dl.queryAll(By.css(CLS_NOT)).length).toBe(0);
-  });
+  }));
 
-  it('should be specify then & else tempatel', () => {
+  it('should be specify then & else tempatel', fakeAsync(() => {
     context.srv.setFull(false);
     context.srv.setRole(['user']);
     context.role = 'admin';
+    fixture.detectChanges();
+    tick();
     fixture.detectChanges();
     expect(dl.queryAll(By.css('.thenBlock')).length).toBe(0);
     expect(dl.queryAll(By.css('.elseBlock')).length).toBe(1);
     context.role = 'user';
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css('.thenBlock')).length).toBe(1);
     expect(dl.queryAll(By.css('.elseBlock')).length).toBe(0);
-  });
+  }));
 
   describe('#except', () => {
     beforeEach(() => {
@@ -91,32 +113,52 @@ describe('acl-if: directive', () => {
       context.role = 'admin';
     });
     describe('with true', () => {
-      it('should be show when user does not a user role', () => {
+      it('should be show when user does not a user role', fakeAsync(() => {
         context.except = true;
+        fixture.detectChanges();
+        tick();
         fixture.detectChanges();
         context.srv.setRole(['user']);
+        fixture.detectChanges();
+        tick();
+        fixture.detectChanges();
         expect(dl.queryAll(By.css('.exceptBlock')).length).toBe(1);
-      });
-      it('should be hide when user has a admin role', () => {
+      }));
+      it('should be hide when user has a admin role', fakeAsync(() => {
         context.except = true;
         fixture.detectChanges();
+        tick();
+        fixture.detectChanges();
         context.srv.setRole(['admin']);
+        fixture.detectChanges();
+        tick();
+        fixture.detectChanges();
         expect(dl.queryAll(By.css('.exceptBlock')).length).toBe(0);
-      });
+      }));
     });
     describe('with false', () => {
-      it('should be hide when user does not a user role', () => {
+      it('should be hide when user does not a user role', fakeAsync(() => {
         context.except = false;
+        fixture.detectChanges();
+        tick();
         fixture.detectChanges();
         context.srv.setRole(['user']);
+        fixture.detectChanges();
+        tick();
+        fixture.detectChanges();
         expect(dl.queryAll(By.css('.exceptBlock')).length).toBe(0);
-      });
-      it('should be show when user has a admin role', () => {
+      }));
+      it('should be show when user has a admin role', fakeAsync(() => {
         context.except = false;
         fixture.detectChanges();
+        tick();
+        fixture.detectChanges();
         context.srv.setRole(['admin']);
+        fixture.detectChanges();
+        tick();
+        fixture.detectChanges();
         expect(dl.queryAll(By.css('.exceptBlock')).length).toBe(1);
-      });
+      }));
     });
   });
 });
@@ -138,7 +180,8 @@ describe('acl-if: directive', () => {
     <div *aclIf="role; then null; else nullThenElseBlock"></div>
     <ng-template #nullThenElseBlock><span class="nullThenElseBlock"></span></ng-template>
   `,
-  imports: [ACLIfDirective]
+  imports: [ACLIfDirective],
+  standalone: true
 })
 class TestComponent {
   readonly srv = inject(ACLService);
--- packages/acl/src/acl.directive.spec.ts
+++ packages/acl/src/acl.directive.spec.ts
@@ -1,5 +1,5 @@
 import { Component, DebugElement, inject } from '@angular/core';
-import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { ACLDirective } from './acl.directive';
@@ -13,46 +13,54 @@ describe('acl: directive', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [{ provide: ComponentFixtureAutoDetect, useValue: true }]
+      imports: [TestComponent]
     });
     fixture = TestBed.createComponent(TestComponent);
     dl = fixture.debugElement;
     context = dl.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should show when full', () => {
+  it('should show when full', fakeAsync(() => {
     context.srv.setFull(true);
     context.role = 'user';
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(`.${CLS}`)).length).toBe(0);
-  });
+  }));
 
-  it('should hide when not full', () => {
+  it('should hide when not full', fakeAsync(() => {
     context.srv.setFull(false);
     context.role = 'user';
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(`.${CLS}`)).length).toBe(1);
-  });
+  }));
 
-  it('should show when ability', () => {
+  it('should show when ability', fakeAsync(() => {
     context.srv.setAbility([1, 2, 3]);
     context.ability = 2;
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(`.${CLS}`)).length).toBe(0);
-  });
+  }));
 
-  it('should hide when not ability', () => {
+  it('should hide when not ability', fakeAsync(() => {
     context.srv.setAbility([1, 2, 3]);
     context.ability = 4;
     fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
     expect(dl.queryAll(By.css(`.${CLS}`)).length).toBe(1);
-  });
+  }));
 });
 
 @Component({
   template: ` <button [acl]="role" [acl-ability]="ability"></button> `,
-  imports: [ACLDirective]
+  imports: [ACLDirective],
+  standalone: true
 })
 class TestComponent {
   readonly srv = inject(ACLService);
